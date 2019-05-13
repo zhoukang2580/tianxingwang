e2a1bf24e4a2624d677c3e699e6031d164a6ad1f
312633086@qq.com <312633086@qq.com>
2019-05-13 13:45:01 +0800

    bug 修复

--- src/app/account/account-device/account-device.page.ts
+++ src/app/account/account-device/account-device.page.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
 import { IonList } from '@ionic/angular';
-
+import { ApiService } from 'src/app/services/api/api.service';
+import { Observable, merge, of } from 'rxjs';
+import { BaseRequest } from 'src/app/services/api/BaseRequest';
+import { map } from 'rxjs/operators';
+type DeviceItem = {
+  deviceId: string;
+  deviceName: string;
+};
 @Component({
   selector: "app-account-device",
   templateUrl: "./account-device.page.html",
@@ -8,19 +15,42 @@ import { IonList } from '@ionic/angular';
 })
 export class AccountDevicePage implements OnInit {
   toggleChecked = false;
+  devices$: Observable<DeviceItem[]>;
   @ViewChild('deviceList') deviceList: IonList;
-  constructor() { }
+  constructor(private apiService: ApiService) {
 
-  ngOnInit() { }
-  delteDevice() { }
-  itemClick() { }
+  }
+
+  ngOnInit() {
+this.loadDevices();
+  }
+  loadDevices() {
+    const req = new BaseRequest();
+    this.devices$ = merge(of([
+      {
+        deviceId: '1',
+        deviceName: "mac"
+      },
+      {
+        deviceId: '2',
+        deviceName: "Iphone"
+      }
+    ]),
+      this.apiService.getResponse<DeviceItem[]>(req).pipe(map(r => r.Data)));
+  }
+  delteDevice() {
+
+  }
+  itemClick() {
+
+  }
   toggleDeleteButton() {
     this.deviceList.closeSlidingItems();
     setTimeout(() => {
       this.toggleChecked = !this.toggleChecked;
     }, this.toggleChecked ? 300 : 0);
   }
-  onSlidingItemDrag(){
-    this.toggleChecked=false;
+  onSlidingItemDrag() {
+    this.toggleChecked = false;
   }
 }
