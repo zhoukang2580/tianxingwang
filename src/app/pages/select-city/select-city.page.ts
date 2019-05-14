142125699ddddb79b7386851ca059291acbb035c
东美在线 <dongmeizaixian@dongmeizaixiandeMacBook-Pro.local>
2019-05-14 17:55:17 +0800

    选择城市，页面返回

--- src/app/pages/select-city/select-city.page.ts
+++ src/app/pages/select-city/select-city.page.ts
@@ -1,17 +1,18 @@
 import { ActivatedRoute, Router } from '@angular/router';
-import { Component, OnInit ,HostBinding} from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import { SelectCityService } from './select-city.service';
 import { trigger, transition, animate, style } from '@angular/animations';
 import { HttpClient } from '@angular/common/http';
+import { NavController } from '@ionic/angular';
 
 
 @Component({
   selector: 'app-select-city',
   templateUrl: './select-city.page.html',
   styleUrls: ['./select-city.page.scss'],
-  animations:[
-    trigger('fadeout',[
-      transition(":leave",animate('0.5s ease-out', style({ left: '-100%' })))
+  animations: [
+    trigger('fadeout', [
+      transition(":leave", animate('0.5s ease-out', style({ left: '-100%' })))
     ])
   ]
 })
@@ -22,24 +23,21 @@ export class SelectCityPage implements OnInit {
   @HostBinding("@fadeout")
   fadeout;
   constructor(private router: Router, private route: ActivatedRoute,
-    private http:HttpClient,
-     private cityService: SelectCityService) {
+    private navCtrl: NavController,
+    private cityService: SelectCityService) {
     this.title = this.cityService.extra && this.cityService.extra.title;
   }
 
   ngOnInit() {
-    
+
     this.items = this.cityService.getCities();
     this.viewModelItems = this.items;
-    
+
   }
   onItemClick(item: any) {
     this.cityService.setSelectedItem(item);
     console.log(this.cityService.extra);
-    this.router.setUpLocationChangeListener
-    setTimeout(() => {
-      window.history.back();
-    }, 100);
+    this.navCtrl.back();
   }
   onIonChange(evt: CustomEvent) {
     if (this.items) {
