c9d8a473e32af13b94b02a8990adab3bc6df8c1e
570607278@qq.com <570607278@qq.com>
2019-07-08 19:28:51 +0800

    航班查询

--- src/app/flight/book-flight/book-flight.page.ts
+++ src/app/flight/book-flight/book-flight.page.ts
@@ -106,25 +106,30 @@ export class BookFlightPage implements OnInit, OnDestroy, AfterViewInit {
     this.totalFlyDays = 4;
   }
   async initFlightCities() {
-    this.fromCity = this.vmFromCity = await this.storage.get("fromCity");
-    this.toCity = this.vmToCity = await this.storage.get("toCity");
-    const cities = await this.flightService.getAllLocalAirports();
-    if (cities && cities.length) {
-      // console.log(cities);
-      this.vmFromCity = this.fromCity = cities.find(
-        c => c.Code.toUpperCase() == "BJS"
-      );
-      this.vmToCity = this.toCity = cities.find(
-        c => c.Code.toUpperCase() == "SHA"
-      );
+    this.fromCity = this.vmFromCity = {} as any;
+    this.fromCity.Nickname = this.fromCity.CityName = this.vmFromCity.CityName =
+      "北京";
+    this.vmFromCity.Code = this.fromCity.Code = "BJS";
+    this.toCity = this.vmToCity = {} as any;
+    this.toCity.Nickname = this.toCity.CityName = this.vmToCity.CityName =
+      "上海";
+    this.vmToCity.Code = this.toCity.Code = "SHA";
+    this.fromCity.Tag = this.toCity.Tag = "AirportCity"; // 出发城市，不是出发城市的那个机场
+    const lastFromCity = await this.storage.get("fromCity");
+    const lastToCity = await this.storage.get("toCity");
+    if (!lastFromCity || !lastToCity) {
+      const cities = await this.flightService.getAllLocalAirports();
+      if (cities && cities.length) {
+        this.vmFromCity = this.fromCity = cities.find(
+          c => c.Code.toUpperCase() == "BJS"
+        );
+        this.vmToCity = this.toCity = cities.find(
+          c => c.Code.toUpperCase() == "SHA"
+        );
+      }
     } else {
-      this.fromCity = this.vmFromCity = {} as any;
-      this.fromCity.CityName = this.vmFromCity.CityName = "北京";
-      this.vmFromCity.Code = this.fromCity.Code = "BJS";
-      this.toCity = this.vmToCity = {} as any;
-      this.toCity.CityName = this.vmToCity.CityName = "上海";
-      this.vmToCity.Code = this.toCity.Code = "SHA";
-      this.fromCity.Tag = this.toCity.Tag = "AirportCity"; // 出发城市，不是出发城市的那个机场
+      this.fromCity = this.vmFromCity = lastFromCity;
+      this.toCity = this.vmToCity = lastToCity;
     }
   }
   searchFlight() {
