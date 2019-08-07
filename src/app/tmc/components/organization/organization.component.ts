53d6185fde1b8d26fcc6d1c19fb99b8fd704a975
570607278@qq.com <570607278@qq.com>
2019-08-07 14:17:46 +0800

    组织架构

--- src/app/tmc/components/organization/organization.component.ts
+++ src/app/tmc/components/organization/organization.component.ts
@@ -2,12 +2,17 @@ import { TmcService } from "src/app/tmc/tmc.service";
 import { OrganizationEntity } from "../../../hr/staff.service";
 import { Component, OnInit, ViewChild } from "@angular/core";
 import { ModalController, IonRefresher } from "@ionic/angular";
+import { Storage } from "@ionic/storage";
 import {
   NodeItem,
   TreeOptions,
   TreeCallbacks
 } from "src/app/components/tree-ngx";
-
+const ORGANIZATION_PREFERANCE_KEY = "organization_preferance_key";
+interface LocalOrganizationEntity {
+  data: OrganizationEntity[];
+  lastUpdateTime: number;
+}
 @Component({
   selector: "app-organization",
   templateUrl: "./organization.component.html",
@@ -15,16 +20,23 @@ import {
 })
 export class OrganizationComponent implements OnInit {
   nodeItems: NodeItem<OrganizationEntity>[] = [];
+  originalNodes: OrganizationEntity[] = [];
+  currentSelectedNode: OrganizationEntity;
+  currentSelectedNodePaths: OrganizationEntity[];
+  currentSelectedNodeChildren: OrganizationEntity[];
   filter = "";
   vmKeyword = "";
   isLoading = true;
-  private selectedNode: OrganizationEntity;
   options: TreeOptions;
   callbacks: TreeCallbacks;
+  isTreeMode;
+  private rootDeptName = "部门";
+  private selectedNode: OrganizationEntity;
   @ViewChild(IonRefresher) ionRefresher: IonRefresher;
   constructor(
     private modalCtrl: ModalController,
-    private tmcService: TmcService
+    private tmcService: TmcService,
+    private storage: Storage
   ) {}
   async back() {
     const t = await this.modalCtrl.getTop();
@@ -34,8 +46,90 @@ export class OrganizationComponent implements OnInit {
   }
   doSearch() {
     this.filter = (this.vmKeyword || "").trim();
+    if (!this.isTreeMode) {
+      if (this.filter) {
+        this.isLoading = true;
+        setTimeout(() => {
+          this.currentSelectedNodeChildren = this.originalNodes.filter(it =>
+            it.Name.includes(this.filter)
+          );
+        }, 100);
+      } else {
+        this.initNodes();
+      }
+      this.isLoading = false;
+    }
   }
-  ngOnInit() {
+  onNextHierachy(node: OrganizationEntity) {
+    this.currentSelectedNode = node;
+    this.currentSelectedNodePaths = this.getParents(node);
+    this.currentSelectedNodeChildren = this.getChildren(node);
+  }
+  private initNodes() {
+    const root = this.findRoot();
+    // console.log(root);
+    if (root) {
+      if (!root.Name) {
+        root.Name = this.rootDeptName;
+      }
+      this.onNextHierachy(root);
+    }
+  }
+  private findRoot() {
+    if (!this.originalNodes || this.originalNodes.length == 0) {
+      return null;
+    }
+    let n = this.originalNodes[0];
+    if (!n.Parent) {
+      return n;
+    }
+    while (n && n.Parent) {
+      const temp = n;
+      n = this.originalNodes.find(it => it.Id == n.ParentId);
+      if (!n || !n.Parent) {
+        return n ? n : temp && temp.Parent;
+      }
+    }
+  }
+  getChildren(node: OrganizationEntity) {
+    if (!node) {
+      return [];
+    }
+    return this.originalNodes.filter(it => it.ParentId == node.Id);
+  }
+  private getParents(node: OrganizationEntity) {
+    const result: OrganizationEntity[] = [];
+    if (!node) {
+      return result;
+    }
+    let root = this.findRoot();
+    if (node.Id == root.Id) {
+      if (!node.Name) {
+        node.Name = this.rootDeptName;
+      }
+      return [node];
+    }
+    result.push(node);
+    let p = node.Parent;
+    while (p) {
+      console.log(p);
+      result.unshift(p);
+      p = this.originalNodes.find(it => it.Id == p.Id);
+    }
+    root = result[0];
+    if (root) {
+      if (!root.Name) {
+        root.Name = this.rootDeptName;
+      }
+    }
+    return result;
+  }
+  async changeMode() {
+    this.isTreeMode = !this.isTreeMode;
+    this.doRefresh();
+  }
+  async ngOnInit() {
+    this.isTreeMode = await this.storage.get(ORGANIZATION_PREFERANCE_KEY);
     this.callbacks = {
       nameClick: (item: NodeItem<OrganizationEntity>) => {
         this.selectedNode = item.item;
@@ -49,11 +143,37 @@ export class OrganizationComponent implements OnInit {
       },
       toggle: (item: NodeItem<OrganizationEntity>) => {}
     };
-    this.doRefresh();
+    await this.doRefresh();
+    this.initNodes();
   }
-  async doRefresh() {
+  onItemSelect(node: OrganizationEntity) {
+    this.selectedNode = node;
+    this.back();
+  }
+  async doRefresh(forceFetch = false) {
     this.isLoading = true;
-    this.nodeItems = (await this.tmcService.getOrganizations()).map(item => {
+    const local: LocalOrganizationEntity = await this.storage.get(
+      ORGANIZATION_PREFERANCE_KEY
+    );
+    if (
+      !forceFetch &&
+      local &&
+      Date.now() - local.lastUpdateTime < 1 * 60 * 1000 &&
+      local.data &&
+      local.data.length
+    ) {
+      this.originalNodes = local.data;
+    } else {
+      this.originalNodes = [];
+    }
+    if (this.originalNodes.length == 0) {
+      this.originalNodes = (await this.tmcService.getOrganizations()).map(
+        it => {
+          return { ...it, ParentId: it && it.Parent.Id };
+        }
+      );
+    }
+    this.nodeItems = this.originalNodes.map(item => {
       return {
         id: item.Id,
         item: item,
