1c3de99197c25aedefecba927d4b409dc4adcac6
570607278@qq.com <570607278@qq.com>
2019-10-09 18:23:59 +0800

    酒店信息

--- src/app/hotel/models/HotelDetailEntity.ts
+++ src/app/hotel/models/HotelDetailEntity.ts
@@ -1,3 +1,45 @@
-export class HotelDetailEntity{
-    
+import { BaseVariablesEntity } from "src/app/tmc/models/BaseVariablesEntity";
+import { HotelEntity } from './HotelEntity';
+
+export class HotelDetailEntity extends BaseVariablesEntity {
+  /// <summary>
+  /// 酒店
+  /// </summary>
+  Hotel: HotelEntity;
+  /// <summary>
+  /// 语言版本
+  /// </summary>
+  Lang: string;
+
+  /// <summary>
+  /// 标签
+  /// Hotel
+  ///     Creditcards
+  ///     IntroEditor
+  ///     Description
+  ///     AirportPickUpService
+  ///     Traffic
+  ///     Surroundings
+  ///     Features
+  /// </summary>
+  Tag: string;
+  /// <summary>
+  ///
+  /// </summary>
+  Number: string;
+  /// <summary>
+  /// Creditcards 酒店支持的信用卡
+  /// IntroEditor 简介
+  /// Description 描述
+  /// AirportPickUpService 接机服务
+  /// Traffic 周边交通
+  /// Surroundings 周边信息
+  /// Features 特色信息
+  /// </summary>
+  Name: string;
+
+  /// <summary>
+  /// 描述
+  /// </summary>
+  Description: string;
 }
