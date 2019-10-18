036899a570034073ffee5403b01e96c2c5d05d91
570607278@qq.com <570607278@qq.com>
2019-10-18 14:26:03 +0800

    order

--- src/app/tmc/models/BaseVariablesEntity.ts
+++ src/app/tmc/models/BaseVariablesEntity.ts
@@ -1,9 +1,9 @@
 import { BaseEntity } from './BaseEntity';
 
-export class BaseVariablesEntity extends BaseEntity{
+export class BaseVariablesEntity extends BaseEntity {
   Variables: string;
   VariablesJsonObj: any;
-  GetVariable<T>(key: string) {
+  public GetVariable<T>(key: string) {
     this.VariablesJsonObj = this.VariablesJsonObj || JSON.parse(this.Variables);
     return this.VariablesJsonObj[key] as T
   }
