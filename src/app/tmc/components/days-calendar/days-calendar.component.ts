4f7d8e109d426e206f9deea31e41148bca6af85b
570607278@qq.com <570607278@qq.com>
2019-10-27 20:48:24 +0800

    日历

--- src/app/tmc/components/days-calendar/days-calendar.component.ts
+++ src/app/tmc/components/days-calendar/days-calendar.component.ts
@@ -47,6 +47,7 @@ export class DaysCalendarComponent implements OnInit, AfterViewInit, OnDestroy {
       if (days && days.length) {
         const selectedDate = days.find(it => it.selected);
         this.initDays(days[0].date, selectedDate);
+        this.moveDateToView();
       } else {
         this.initDays(moment().format("YYYY-MM-DD"))
       }
@@ -80,6 +81,9 @@ export class DaysCalendarComponent implements OnInit, AfterViewInit, OnDestroy {
     this.calenderClick.emit();
   }
   ngAfterViewInit() {
+    this.moveDateToView();
+  }
+  private moveDateToView() {
     setTimeout(() => {
       const d = this.days.find(it => it.selected);
       if (d) {
@@ -100,45 +104,43 @@ export class DaysCalendarComponent implements OnInit, AfterViewInit, OnDestroy {
         index = i;
       }
     }
-    this.domCtrl.write(ts => {
-      if (!this.daysEle || !this.daysEle.nativeElement) {
-        // 如果这里的日历找不到对应的日期，结束
-        if (!this.days.find(item => item.date == day.date)) {
-          const ele = this.daysEle.nativeElement.querySelector(".active");
-          // console.log("日期变更停止",ele);
-          if (ele) {
-            this.render.removeClass(ele, "active");
-          }
+    if (!this.daysEle || !this.daysEle.nativeElement) {
+      // 如果这里的日历找不到对应的日期，结束
+      if (!this.days.find(item => item.date == day.date)) {
+        const ele = this.daysEle.nativeElement.querySelector(".active");
+        // console.log("日期变更停止",ele);
+        if (ele) {
+          this.render.removeClass(ele, "active");
         }
-        return;
       }
-      const daysEle = this.daysEle.nativeElement;
-      let selectedEle;
-      // console.dir(this.dayItems);
-      if (this.dayItems && this.dayItems.length) {
-        this.dayItems.forEach(item => {
-          // console.log(item.nativeElement.getAttribute("date"));
-          if (item.nativeElement.getAttribute("date") == day.date) {
-            selectedEle = item.nativeElement;
-            this.render.addClass(item.nativeElement, "active");
-          } else {
-            this.render.removeClass(item.nativeElement, "active");
-          }
-        });
-      }
-      if (daysEle && selectedEle) {
-        const clientRect = selectedEle.getBoundingClientRect();
-        // console.dir(daysEle);
-        const dist =
-          clientRect.width / 2 + clientRect.left - this.plt.width() / 2;
-        // console.dir(dist);
-        daysEle.scrollBy({
-          left: dist,
-          top: 0,
-          behavior: "smooth"
-        });
-      }
-    });
+      return;
+    }
+    const daysEle = this.daysEle.nativeElement;
+    let selectedEle;
+    // console.dir(this.dayItems);
+    if (this.dayItems && this.dayItems.length) {
+      this.dayItems.forEach(item => {
+        // console.log(item.nativeElement.getAttribute("date"));
+        if (item.nativeElement.getAttribute("date") == day.date) {
+          selectedEle = item.nativeElement;
+          this.render.addClass(item.nativeElement, "active");
+        } else {
+          this.render.removeClass(item.nativeElement, "active");
+        }
+      });
+    }
+    if (daysEle && selectedEle) {
+      const clientRect = selectedEle.getBoundingClientRect();
+      // console.dir(daysEle);
+      const dist =
+        clientRect.width / 2 + clientRect.left - this.plt.width() / 2;
+      // console.dir(dist);
+      daysEle.scrollBy({
+        left: dist,
+        top: 0,
+        behavior: "smooth"
+      });
+    }
     if (byUser) {
       this.itemSelected.emit(day);
     }
