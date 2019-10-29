102f58cf891e94e97a5c840db59cd8f5d38f07c3
东美在线 <dongmeizaixian@dongmeizaixiandeMacBook-Pro.local>
2019-10-29 15:48:54 +0800

    bug修复

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { environment } from "./environments/environment";
 
 if (environment.production) {
   enableProdMode();
+  
+}else{
   if (window["cordova"]) {
     if (window["VConsole"]) {
       var vConsole = new window["VConsole"]();
