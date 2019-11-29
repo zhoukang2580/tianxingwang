793fc42fda0892255b0c8991ec799d724658eb55
570607278@qq.com <570607278@qq.com>
2019-11-29 12:25:12 +0800

    lazyload image

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -1,21 +1,16 @@
-import { ConfigEntity } from './../services/config/config.entity';
-import { ConfigService } from 'src/app/services/config/config.service';
-import { AppHelper } from 'src/app/appHelper';
+import { LazyloadDirective } from './lazyload.directive';
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CropAvatarDirective } from "./crop-avatar.directive";
 import { ShowtipDirective } from "./showtip.directive";
 import { AutoGrowDirective } from "./auto-grow.directive";
-import { SetErrorImageProps, Attributes } from './ng-lazyload-image/types';
-import { scrollPreset } from './ng-lazyload-image/scroll-preset';
-import { LazyloadDirective } from './lazyload.directive';
 
 @NgModule({
   declarations: [
     CropAvatarDirective,
     ShowtipDirective,
     AutoGrowDirective,
-    LazyloadDirective,
+    LazyloadDirective
   ],
   imports: [CommonModule],
   exports: [
