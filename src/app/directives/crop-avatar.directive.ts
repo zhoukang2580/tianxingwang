049621a9c5d5977557ad89b537a90bb08b11fea9
570607278@qq.com <570607278@qq.com>
2019-12-05 12:21:02 +0800

    图片上传

--- src/app/directives/crop-avatar.directive.ts
+++ src/app/directives/crop-avatar.directive.ts
@@ -1,3 +1,4 @@
+import { WebView } from '@ionic-native/ionic-webview/ngx';
 import { Directive, HostListener, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppHelper } from '../appHelper';
@@ -8,7 +9,7 @@ import { NavController } from '@ionic/angular';
 })
 export class CropAvatarDirective {
 
-  constructor(private navCtrl: NavController, private sender: ElementRef) {
+  constructor(private navCtrl: NavController, private sender: ElementRef, private webView: WebView) {
     console.dir(sender);
   }
   @HostListener("click", ['$event'])
@@ -27,24 +28,25 @@ export class CropAvatarDirective {
         const files = (evt.target as HTMLInputElement).files;
         const file = files[0];
         if (file) {
-          // window.URL=window.URL||window['webkitURL'];
-          // const objectURL = window.URL.createObjectURL(file);
-          // AppHelper.setRouteData(objectURL);
-          // const method = self.sender.nativeElement.attributes["upload-method"].value;
-          // this.navCtrl.navigateForward([AppHelper.getRoutePath('crop-avatar'), { 'cropAvatar': "cropAvatar", "method": method, "fileName": file.name }], { animated: false }).then(() => {
-          //   fileEle.value = null;
-          // });
-          const fr = new FileReader();
-          fr.onload = () => {
-            AppHelper.setRouteData(fr.result);
-            const method=self.sender.nativeElement.attributes["upload-method"].value;
-            this.navCtrl.navigateForward([AppHelper.getRoutePath('crop-avatar'),{'cropAvatar':"cropAvatar","method":method,"fileName":file.name}],{animated:false}).then(() => {
+          if(AppHelper.isApp()){
+            window.URL = window.URL || window['webkitURL'];
+            const objectURL = window.URL.createObjectURL(file);
+            AppHelper.setRouteData(this.webView.convertFileSrc(objectURL));
+            const method = self.sender.nativeElement.attributes["upload-method"].value;
+            this.navCtrl.navigateForward([AppHelper.getRoutePath('crop-avatar'), { 'cropAvatar': "cropAvatar", "method": method, "fileName": file.name }], { animated: false }).then(() => {
               fileEle.value = null;
             });
-          }
-          fr.readAsDataURL(file);
-          fr.onprogress=_=>{
-            console.log("正在读取文件，请稍后...",_);
+
+          }else{
+            const fr = new FileReader();
+            fr.onload = () => {
+              AppHelper.setRouteData(fr.result);
+              const method=self.sender.nativeElement.attributes["upload-method"].value;
+              this.navCtrl.navigateForward([AppHelper.getRoutePath('crop-avatar'),{'cropAvatar':"cropAvatar","method":method,"fileName":file.name}],{animated:false}).then(() => {
+                fileEle.value = null;
+              });
+            }
+            fr.readAsDataURL(file);
           }
         }
       }
