8e8adcdd7cbd7186b18e1545a74cc2c25c51ed97
570607278@qq.com <570607278@qq.com>
2019-12-09 13:29:45 +0800

    微信程序，禁止下滑

--- src/app/directives/refresher.directive.ts
+++ src/app/directives/refresher.directive.ts
@@ -1,29 +1,38 @@
-import { AppHelper } from './../appHelper';
 import { Platform } from '@ionic/angular';
-import { OnInit, OnDestroy } from '@angular/core';
+import { OnInit, OnDestroy, AfterViewInit } from '@angular/core';
 import { ElementRef } from '@angular/core';
 import { Directive } from '@angular/core';
 
 @Directive({
   selector: 'ion-refresher'
 })
-export class RefresherDirective implements OnInit, OnDestroy {
-  private isWechatH5 = /micromessenger/gi.test(window.navigator.userAgent) || AppHelper.isWechatH5();
+export class RefresherDirective implements OnInit, AfterViewInit, OnDestroy {
+  private isWechatH5 = /micromessenger/gi.test(window.navigator.userAgent);
   private scroller: HTMLElement;
   private startY = 0;
   private scrollTop = 0;
+  private content: HTMLIonContentElement;
   constructor(private el: ElementRef<HTMLElement>, private plt: Platform) { }
   private scrollStart(evt: TouchEvent) {
     this.startY = evt.touches[0] && evt.touches[0].clientY;
     this.scrollTop = this.scroller && this.scroller.scrollTop;
-    console.log("scrollStart", this.scrollTop);
+    if (!this.scroller) {
+      this.content = this.el.nativeElement.closest("ion-content");
+      if (this.content) {
+        this.content.getScrollElement().then(el => {
+          this.scroller = el;
+        });
+      }
+    }
   }
   private scrollFn(evt: TouchEvent) {
+
     // console.log(evt);
     const delta = evt.touches[0] && evt.touches[0].clientY - this.startY;
     if (evt) {
       if (evt.cancelable) {
-        console.log("detal", delta,this.scrollTop,"preventDefault");
+        window['_isPreventDefault'] = delta >= 0 && this.scrollTop <= 0;
+        console.log("detal", delta, this.scrollTop, window['_isPreventDefault'])
         if (delta >= 0 && this.scrollTop <= 0) {
           // console.log("detal", delta)
           evt.preventDefault();
@@ -34,31 +43,30 @@ export class RefresherDirective implements OnInit, OnDestroy {
   ngOnDestroy() {
     document.body.removeEventListener("touchstart", this.scrollStart.bind(this));
     document.body.removeEventListener("touchmove", this.scrollFn.bind(this));
+    document.body.removeEventListener("touchend", this.touchend.bind(this));
+  }
+  private touchend() {
+    window['_isPreventDefault'] = false;
+  }
+  ngAfterViewInit() {
+    setTimeout(async () => {
+      if (this.el.nativeElement) {
+        // console.dir(this.el);
+        this.el.nativeElement['pullFactor'] = 1.1;
+      }
+      const isAndroidWechatH5 = this.plt.is('android') && this.isWechatH5;
+      if (isAndroidWechatH5) {
+        // console.log("IonContentDirective", this.el, "是否在微信浏览器内部：", this.isWechatH5);
+        this.content = this.el.nativeElement.closest("ion-content");
+        if (this.content) {
+          this.scroller = await this.content.getScrollElement();
+          document.body.addEventListener("touchmove", this.scrollFn.bind(this), false);
+          document.body.addEventListener("touchstart", this.scrollStart.bind(this), false);
+          document.body.addEventListener("touchend", this.touchend.bind(this), false);
+        }
+      }
+    }, 1000);
   }
   ngOnInit() {
-    // console.log("是否在微信浏览器内部：", this.isWechatH5)
-    if (this.el.nativeElement) {
-      // console.dir(this.el);
-      this.el.nativeElement['pullFactor'] = 1.1;
-      if (this.el.nativeElement['ionRefresh']) {
-
-      }
-    }
-    const isAndroidWechatH5 = this.plt.is('android') && this.isWechatH5;
-    if (isAndroidWechatH5) {
-      const content = this.el.nativeElement.closest("ion-content");
-      console.log("IonContentDirective", this.el, "是否在微信浏览器内部：", this.isWechatH5, content);
-      if (content) {
-        setTimeout(async () => {
-          this.scroller = await content.getScrollElement();
-          console.log("this.scroller", this.scroller);
-          // this.overscroll(this.scroller.querySelector(".scroll"));
-          document.body.addEventListener("touchmove", this.scrollFn.bind(this), {
-            passive: false,
-          });
-          document.body.addEventListener("touchstart", this.scrollStart.bind(this));
-        }, 1000);
-      }
-    }
   }
 }
