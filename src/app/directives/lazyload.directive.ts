4b5bd66dd084c914ec97515bdd6b7330ebdb3504
huangmanbiao <570607278@qq.com>
2020-01-09 14:13:40 +0800

    '图片延迟加载，钉钉浏览器不加载图片bug修复'

--- src/app/directives/lazyload.directive.ts
+++ src/app/directives/lazyload.directive.ts
@@ -1,21 +1,34 @@
-import { DomSanitizer } from '@angular/platform-browser';
-import { OnInit, OnChanges, Input, OnDestroy, Renderer2, ElementRef, AfterContentInit } from '@angular/core';
-import { Directive } from '@angular/core';
-import { ImageRecoverService } from '../services/imageRecover/imageRecover.service';
+import { AppHelper } from "./../appHelper";
+import { DomSanitizer } from "@angular/platform-browser";
+import {
+  OnInit,
+  OnChanges,
+  Input,
+  OnDestroy,
+  Renderer2,
+  ElementRef,
+  AfterContentInit
+} from "@angular/core";
+import { Directive } from "@angular/core";
+import { ImageRecoverService } from "../services/imageRecover/imageRecover.service";
 
 @Directive({
-  selector: '[lazyLoad]'
+  selector: "[lazyLoad]"
 })
-export class LazyloadDirective implements OnInit, OnChanges, OnDestroy, AfterContentInit {
+export class LazyloadDirective
+  implements OnInit, OnChanges, OnDestroy, AfterContentInit {
   private io: IntersectionObserver;
   @Input() lazyLoad: string;
   @Input() defaultImage: string;
-  constructor(private imageRecoverService: ImageRecoverService, private el: ElementRef<HTMLDivElement | HTMLImageElement>, private render: Renderer2) { }
+  constructor(
+    private imageRecoverService: ImageRecoverService,
+    private el: ElementRef<HTMLDivElement | HTMLImageElement>,
+    private render: Renderer2
+  ) {}
   ngOnChanges() {
     this.addIO();
   }
-  ngAfterContentInit() {
-  }
+  ngAfterContentInit() {}
   ngOnInit() {
     this.setupImageRecover();
     this.setDefaultImage();
@@ -29,9 +42,12 @@ export class LazyloadDirective implements OnInit, OnChanges, OnDestroy, AfterCon
     // Do something
     // console.log("settup", atter)
     // atter.imagePath = null;
-    if (!this.el.nativeElement.dataset || !this.el.nativeElement.dataset['isInitialized']) {
+    if (
+      !this.el.nativeElement.dataset ||
+      !this.el.nativeElement.dataset["isInitialized"]
+    ) {
       this.imageRecoverService.initialize(this.el.nativeElement);
-      this.el.nativeElement.dataset['isInitialized'] = "isInitialized";
+      this.el.nativeElement.dataset["isInitialized"] = "isInitialized";
     }
   }
   ngOnDestroy() {
@@ -42,11 +58,15 @@ export class LazyloadDirective implements OnInit, OnChanges, OnDestroy, AfterCon
     url = `${url}`.replace(/\?v=\d+/g, "");
     // console.log('load url:', url);
     if (this.el.nativeElement instanceof HTMLDivElement) {
-      this.render.setStyle(this.el.nativeElement, 'background-image', `url(${url})`);
+      this.render.setStyle(
+        this.el.nativeElement,
+        "background-image",
+        `url(${url})`
+      );
       // this.render.setProperty(this.el.nativeElement,'backgroundImage',`${src || this.lazyLoad}`);
       // this.el.nativeElement.style.backgroundImage=`${src || this.lazyLoad}`;
     } else {
-      this.render.setProperty(this.el.nativeElement, 'src', url);
+      this.render.setProperty(this.el.nativeElement, "src", url);
       // this.el.nativeElement.src = src || this.lazyLoad;
     }
   }
@@ -60,22 +80,34 @@ export class LazyloadDirective implements OnInit, OnChanges, OnDestroy, AfterCon
     if (!this.lazyLoad) {
       return;
     }
-    if ('IntersectionObserver' in window && "IntersectionObserverEntry" in window) {
-      console.log('当前浏览器支持：IntersectionObserver');
+    // if (AppHelper.isDingtalkH5()) {
+    //   setTimeout(() => this.load(), 200);
+    //   return;
+    // }
+    if (
+      "IntersectionObserver" in window &&
+      "IntersectionObserverEntry" in window
+    ) {
+      console.log("当前浏览器支持：IntersectionObserver");
       this.removeIO();
       this.io = new IntersectionObserver(data => {
         // because there will only ever be one instance
         // of the element we are observing
         // we can just use data[0]
-        if (data.find(it=>it.isIntersecting)) {
+        if (data.find(it => it.isIntersecting)) {
           this.load();
           this.removeIO();
+        } else if (AppHelper.isDingtalkH5()) {
+          setTimeout(() => {
+            this.load();
+            this.removeIO();
+          }, 200);
         }
       });
       this.io.observe(this.el.nativeElement);
     } else {
       // fall back to setTimeout for Safari and IE
-      console.error('当前浏览器不支持：IntersectionObserver');
+      console.error("当前浏览器不支持：IntersectionObserver");
       setTimeout(() => this.load(), 200);
     }
   }
