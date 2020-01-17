242ea258ef3dc1309e1e94f49527874fd1a55116
huangmanbiao <570607278@qq.com>
2020-01-17 19:00:00 +0800

    '订单列表'

---
+++ src/app/agent/agent.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from "@angular/core";
+import { ApiService } from "../services/api/api.service";
+import { TmcEntity, TmcService } from "../tmc/tmc.service";
+import { RequestEntity } from "../services/api/Request.entity";
+import { IdentityEntity } from "../services/identity/identity.entity";
+import { IdentityService } from "../services/identity/identity.service";
+import { OrderModel } from '../order/models/OrderModel';
+
+@Injectable({
+  providedIn: "root"
+})
+export class AgentService {
+  tmc: TmcEntity;
+  constructor(
+    private apiService: ApiService,
+    private identityService: IdentityService,
+    private tmcService: TmcService
+  ) {}
+  async onSelect(item: TmcEntity) {
+    let res = false;
+    const req = new RequestEntity();
+    req.Method = "TmcApiHomeUrl-Agent-SelectTmc";
+    req.Data = {
+      TmcId: item.Id
+    };
+    req.IsShowLoading = true;
+    const result = await this.apiService
+      .getPromiseData<IdentityEntity>(req)
+      .catch(e => {
+        console.log(e);
+        return null;
+      });
+    if (result && result.Numbers && result.Numbers.TmcId) {
+      const identityEntity = await this.identityService.getIdentityAsync();
+      this.identityService.setIdentity({
+        ...identityEntity,
+        ...result
+      });
+      res = true;
+      this.tmc = await this.tmcService.getTmc(true);
+    }
+    return res;
+  }
+  queryTmc(name: string) {
+    const req = new RequestEntity();
+    req.Method = "TmcApiHomeUrl-Agent-QueryTmc";
+    req.Data = {
+      Name: name
+    };
+    return this.apiService.getResponse<TmcEntity[]>(req);
+  }
+}
---
+++ src/app/agent/order-list/order-list.page.ts
@@ -0,0 +1,85 @@
+import { Router } from "@angular/router";
+import { AgentService } from "./../agent.service";
+import { TmcService } from "src/app/tmc/tmc.service";
+import { ProductItemType } from "./../../tmc/models/ProductItems";
+import { ORDER_TABS } from "./../../order/product-list/product-list.page";
+import { finalize, catchError, map } from "rxjs/operators";
+import { Subscription, of } from "rxjs";
+import { OrderService } from "./../../order/order.service";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { OrderModel } from "src/app/order/models/OrderModel";
+import { OrderEntity } from "src/app/order/models/OrderEntity";
+import { ProductItem } from "src/app/tmc/models/ProductItems";
+import { AppHelper } from "src/app/appHelper";
+
+@Component({
+  selector: "app-order-list",
+  templateUrl: "./order-list.page.html",
+  styleUrls: ["./order-list.page.scss"]
+})
+export class OrderListPage implements OnInit, OnDestroy {
+  private searchCondition: OrderModel;
+  private loadSubscription = Subscription.EMPTY;
+  tab: ProductItem; //
+  tabs: ProductItem[];
+  loadDataErrmsg = "";
+  isLoading = false;
+  orderList: OrderEntity[];
+  constructor(
+    private orderService: OrderService,
+    private agentService: AgentService,
+    private router: Router
+  ) {}
+  onActiveTab(tab: ProductItem) {
+    this.tab = tab;
+    this.doRefresh();
+  }
+  ngOnInit() {
+    this.tabs = ORDER_TABS.filter(it => it.isDisplay).filter(
+      it =>
+        it.value != ProductItemType.waitingApprovalTask &&
+        it.value != ProductItemType.more
+    );
+    this.tab = ORDER_TABS.find(it => it.value == ProductItemType.plane);
+    this.onActiveTab(this.tab);
+  }
+  goToDetailPage(orderId: string) {
+    this.router.navigate([AppHelper.getRoutePath("order-detail")], {
+      queryParams: {
+        tab: JSON.stringify(this.tab),
+        orderId: orderId
+      }
+    });
+  }
+  async doRefresh() {
+    this.searchCondition = new OrderModel();
+    // this.searchCondition.Tmc = this.agentService.tmc;
+    this.orderList = [];
+    this.loadSubscription.unsubscribe();
+    this.loadDataErrmsg = "";
+    this.loadMore();
+  }
+  ngOnDestroy() {
+    this.loadSubscription.unsubscribe();
+  }
+  loadMore() {
+    this.loadSubscription = this.orderService
+      .getOrderList(this.searchCondition)
+      .pipe(
+        finalize(() => {
+          setTimeout(() => {
+            this.isLoading = false;
+          }, 200);
+        }),
+        map(res => (res && res.Data && res.Data.Orders) || []),
+        catchError(e => {
+          this.loadDataErrmsg = e.Message || e;
+          return of([]);
+        })
+      )
+      .subscribe((res: OrderEntity[]) => {
+        this.orderList = res;
+      });
+  }
+  onFilter() {}
+}
