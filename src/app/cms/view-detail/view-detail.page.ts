705c294816a46546b2e2f4165f4f51a319f6e03c
huangmanbiao <570607278@qq.com>
2020-02-03 18:04:05 +0800

    css

--- src/app/cms/view-detail/view-detail.page.ts
+++ src/app/cms/view-detail/view-detail.page.ts
@@ -1,8 +1,14 @@
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from "@angular/router";
 import { environment } from "src/environments/environment";
 import { CmsService, Notice } from "./../cms.service";
 import { DomSanitizer } from "@angular/platform-browser";
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterContentChecked
+} from "@angular/core";
 import { Observable } from "rxjs";
 import { map, tap } from "rxjs/operators";
 import { NavController } from "@ionic/angular";
@@ -12,14 +18,17 @@ import { NavController } from "@ionic/angular";
   templateUrl: "./view-detail.page.html",
   styleUrls: ["./view-detail.page.scss"]
 })
-export class ViewDetailPage implements OnInit {
+export class ViewDetailPage implements OnInit, AfterContentChecked {
+  @ViewChild("detailInfo", { static: false }) detailInfoEle: ElementRef<
+    HTMLElement
+  >;
   notice: Notice;
   constructor(
     private cmsService: CmsService,
     private sanitizer: DomSanitizer,
     private navCtrl: NavController,
     private route: ActivatedRoute
-  ) { }
+  ) {}
   back() {
     this.navCtrl.pop();
   }
@@ -28,12 +37,28 @@ export class ViewDetailPage implements OnInit {
       if (d) {
         const id = d.get("noticeId");
         if (id) {
-          this.notice = await this.cmsService.getNoticeDetail(id).catch(_ => null);
+          this.notice = await this.cmsService
+            .getNoticeDetail(id)
+            .catch(_ => null);
           if (this.notice && this.notice.Url) {
-            this.notice.Url = this.sanitizer.bypassSecurityTrustResourceUrl(this.notice.Url) as string;
+            this.notice.Url = this.sanitizer.bypassSecurityTrustResourceUrl(
+              this.notice.Url
+            ) as string;
           }
         }
       }
-    })
+    });
+  }
+  ngAfterContentChecked() {
+    if (this.detailInfoEle) {
+      const pres = this.detailInfoEle.nativeElement.querySelectorAll("pre");
+      pres.forEach(pre => {
+        // console.log("content checck", pre.classList);
+        if (pre && !pre.classList.contains("normal")) {
+          pre.classList.add("normal");
+          pre.style.whiteSpace = "normal";
+        }
+      });
+    }
   }
 }
