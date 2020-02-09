b55e8c2f1239a170182dafaaddcef9eb0347b59f
huangmanbiao <570607278@qq.com>
2020-02-09 13:36:02 +0800

    bug修复

--- src/hmr.ts
+++ src/hmr.ts
@@ -2,20 +2,35 @@ import { enableDebugTools } from "@angular/platform-browser";
 import { NgModuleRef, ApplicationRef } from "@angular/core";
 import { createNewHosts } from "@angularclass/hmr";
 
+// export const hmrBootstrap = (
+//   module: any,
+//   bootstrap: () => Promise<NgModuleRef<any>>
+// ) => {
+//   let ngModule: NgModuleRef<any>;
+//   module.hot.accept();
+//   bootstrap().then(currentModule => (ngModule = currentModule));
+//   module.hot.dispose(() => {
+//     const appRef: ApplicationRef = ngModule.injector.get(ApplicationRef);
+//     const elements = appRef.components.map(c => c.location.nativeElement);
+//     const removeOldHosts = createNewHosts(elements);
+//     const appComp = appRef.components[0];
+//     enableDebugTools(appComp);
+//     ngModule.destroy();
+//     removeOldHosts();
+//   });
+// };
 export const hmrBootstrap = (
   module: any,
   bootstrap: () => Promise<NgModuleRef<any>>
 ) => {
   let ngModule: NgModuleRef<any>;
   module.hot.accept();
-  bootstrap().then(currentModule => (ngModule = currentModule));
+  bootstrap().then(mod => (ngModule = mod));
   module.hot.dispose(() => {
     const appRef: ApplicationRef = ngModule.injector.get(ApplicationRef);
     const elements = appRef.components.map(c => c.location.nativeElement);
-    const removeOldHosts = createNewHosts(elements);
-    // const appComp = appRef.components[0];
-    // enableDebugTools(appComp);
+    const makeVisible = createNewHosts(elements);
     ngModule.destroy();
-    removeOldHosts();
+    makeVisible();
   });
 };
