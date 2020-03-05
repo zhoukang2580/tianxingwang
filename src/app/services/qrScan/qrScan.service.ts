a2c00041977163f0b580a1ca27927b52783151e2
huangmanbiao <570607278@qq.com>
2020-03-05 12:33:51 +0800

    扫码

--- src/app/services/qrScan/qrScan.service.ts
+++ src/app/services/qrScan/qrScan.service.ts
@@ -1,73 +1,6 @@
-import { Platform } from '@ionic/angular';
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class QrScanService implements IQRScanner {
-  private qrScanner: { [key in keyof IQRScanner]: any };
-  constructor(private plt: Platform) {
-    this.plt.ready().then(() => {
-      this.qrScanner = window['qrScanner'];
-    })
-  }
-  prepare() {
-    return new Promise<IQrScannerStatus>((resolve, reject) => {
-      this.qrScanner.prepare(resolve, reject);
-    });
-  }
-  cancelScan() {
-    return new Promise<void>((resolve, reject) => {
-      this.qrScanner.cancelScan(resolve, reject);
-    })
-  }
-
-  destroy() {
-    return new Promise<void>((resolve, reject) => {
-      this.qrScanner.destroy(resolve, reject);
-    })
-  }
-  enableLight() {
-    return new Promise<void>((resolve, reject) => {
-      this.qrScanner.enableLight(resolve, reject);
-    })
-  }
-  disableLight() {
-    return new Promise<void>((resolve, reject) => {
-      this.qrScanner.enableLight(resolve, reject);
-    })
-  }
-  scan() {
-    return new Promise<string>((resolve, reject) => {
-      this.qrScanner.scan(resolve, reject);
-    })
-  }
-  hide() {
-    return new Promise<IQrScannerStatus>((resolve, reject) => {
-      this.qrScanner.scan(resolve, reject);
-    })
-  }
-  getStatus() {
-    return new Promise<IQrScannerStatus>((resolve, reject) => {
-      this.qrScanner.getStatus(resolve, reject);
-    })
-  }
-  show() {
-    return new Promise<IQrScannerStatus>((resolve, reject) => {
-      this.qrScanner.show(resolve, reject);
-    })
-  }
-  pausePreview() {
-    return new Promise<IQrScannerStatus>((resolve, reject) => {
-      this.qrScanner.pausePreview(resolve, reject);
-    })
-  }
-  openSettings() {
-    return new Promise<IQrScannerStatus>((resolve, reject) => {
-      this.qrScanner.openSettings(resolve, reject);
-    })
-  }
-}
+import { Platform } from "@ionic/angular";
+import { Injectable } from "@angular/core";
+import { Subject, BehaviorSubject, Observable, Subscription } from "rxjs";
 export interface IQRScanner {
   prepare: () => Promise<IQrScannerStatus>;
   cancelScan: () => Promise<void>;
@@ -77,10 +10,129 @@ export interface IQRScanner {
   scan: () => Promise<string>;
   hide: () => Promise<IQrScannerStatus>;
   pausePreview: () => Promise<IQrScannerStatus>;
+  resumePreview: () => Promise<IQrScannerStatus>;
   show: () => Promise<IQrScannerStatus>;
   getStatus: () => Promise<IQrScannerStatus>;
   openSettings: () => Promise<IQrScannerStatus>;
 }
+@Injectable({
+  providedIn: "root"
+})
+export class QrScanService implements IQRScanner {
+  private qrScanner: { [key in keyof IQRScanner]: any };
+  private subscription = Subscription.EMPTY;
+  private scanResultSource: Subject<string>;
+  isScanSetup = false;
+  constructor(private plt: Platform) {
+    this.plt.ready().then(() => {
+      this.qrScanner = window["qrScanner"];
+    });
+    this.scanResultSource = new BehaviorSubject("");
+  }
+  prepare() {
+    return new Promise<IQrScannerStatus>((resolve, reject) => {
+      this.qrScanner.prepare(resolve, reject);
+    });
+  }
+  cancelScan() {
+    this.isScanSetup = false;
+    return new Promise<void>((resolve, reject) => {
+      this.qrScanner.cancelScan(resolve, reject);
+    });
+  }
+
+  destroy() {
+    this.isScanSetup = false;
+    return new Promise<void>((resolve, reject) => {
+      this.qrScanner.destroy(resolve, reject);
+    });
+  }
+  enableLight() {
+    return new Promise<void>((resolve, reject) => {
+      this.qrScanner.enableLight(resolve, reject);
+    });
+  }
+  disableLight() {
+    return new Promise<void>((resolve, reject) => {
+      this.qrScanner.enableLight(resolve, reject);
+    });
+  }
+  async scan() {
+    if (!this.isScanSetup) {
+      this.isScanSetup = true;
+      this.qrScanner.scan(
+        text => {
+          this.scanResultSource.next(text);
+        },
+        err => {
+          this.scanResultSource.error(err);
+        }
+      );
+    } else {
+      let status = await this.getStatus();
+      if (status.authorized == "0") {
+        const s = await this.prepare();
+        if (s.denied == "1") {
+          this.subscription.unsubscribe();
+          return Promise.reject("您拒绝使用相机功能");
+        }
+        if (s.authorized == "0") {
+          await this.openSettings();
+        }
+      }
+      status = await this.getStatus();
+      if (status.authorized == "0") {
+        this.subscription.unsubscribe();
+        return Promise.reject("您尚未允许使用相机功能");
+      }
+      if (status.previewing == "0") {
+        await this.resumePreview();
+      }
+    }
+    this.subscription.unsubscribe();
+    return new Promise<string>((resolve, reject) => {
+      this.subscription = this.scanResultSource.subscribe(
+        text => {
+          resolve(text);
+        },
+        err => {
+          reject(err);
+        }
+      );
+    });
+  }
+  resumePreview() {
+    return new Promise<IQrScannerStatus>((resolve, reject) => {
+      this.qrScanner.resumePreview(resolve, reject);
+    });
+  }
+  pausePreview() {
+    return new Promise<IQrScannerStatus>((resolve, reject) => {
+      this.qrScanner.pausePreview(resolve, reject);
+    });
+  }
+  getStatus() {
+    return new Promise<IQrScannerStatus>((resolve, reject) => {
+      this.qrScanner.getStatus(resolve, reject);
+    });
+  }
+  hide() {
+    return new Promise<IQrScannerStatus>((resolve, reject) => {
+      this.qrScanner.hide(resolve, reject);
+    });
+  }
+  show() {
+    return new Promise<IQrScannerStatus>((resolve, reject) => {
+      this.qrScanner.show(resolve, reject);
+    });
+  }
+  openSettings() {
+    return new Promise<IQrScannerStatus>((resolve, reject) => {
+      this.qrScanner.openSettings(resolve, reject);
+    });
+  }
+}
+
 export type ZeroOne = "0" | "1";
 export interface IQrScannerStatus {
   authorized: ZeroOne;
