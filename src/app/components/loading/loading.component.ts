e705976f3f496a83316132eeaa51b78e9de2a085
东美在线 <dongmeizaixian@dongmeizaixiandeMacBook-Pro.local>
2020-03-10 19:14:42 +0800

    样式

--- src/app/components/loading/loading.component.ts
+++ src/app/components/loading/loading.component.ts
@@ -4,8 +4,10 @@ import {
   OnChanges,
   SimpleChanges,
   Input,
-  HostBinding
+  HostBinding,
+  ElementRef
 } from "@angular/core";
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: "app-loading-comp",
@@ -15,9 +17,14 @@ import {
 export class LoadingComponent implements OnInit, OnChanges {
   @HostBinding("class.loading")
   @Input() loading: boolean;
-  constructor() {}
+  constructor(private el:ElementRef<HTMLElement>,private plt:Platform) {}
 
   ngOnInit() {
+    if(window['hcp']&&this.plt.is("android")){
+      window['hcp'].getStatusBarHeight().then(sh=>{
+        this.el.nativeElement.style.marginTop=-sh+"px";
+      })
+    }
   }
   ngOnChanges(changes: SimpleChanges) {
     // console.log(this.loading, changes.loading);
