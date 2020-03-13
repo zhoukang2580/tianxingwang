2da3ef53944858eae8518f5d8a5c371c0db6493a
huangmanbiao <570607278@qq.com>
2020-03-13 15:26:31 +0800

    国际酒店，选择城市，改为后台查询

--- src/app/hotel-international/select-inter-city/select-inter-city.page.ts
+++ src/app/hotel-international/select-inter-city/select-inter-city.page.ts
@@ -1,6 +1,9 @@
 import { BackButtonComponent } from "./../../components/back-button/back-button.component";
 import { Subscription, of } from "rxjs";
-import { InternationalHotelService } from "./../international-hotel.service";
+import {
+  InternationalHotelService,
+  ISearchTextValue
+} from "./../international-hotel.service";
 import {
   Component,
   OnInit,
@@ -11,7 +14,13 @@ import {
   QueryList,
   ElementRef
 } from "@angular/core";
-import { debounceTime, take, tap, distinctUntilChanged } from "rxjs/operators";
+import {
+  debounceTime,
+  take,
+  tap,
+  distinctUntilChanged,
+  finalize
+} from "rxjs/operators";
 import {
   IonSearchbar,
   IonContent,
@@ -22,15 +31,23 @@ import {
 } from "@ionic/angular";
 import { flyInOut } from "src/app/animations/flyInOut";
 import { TrafficlineEntity } from "src/app/tmc/models/TrafficlineEntity";
-import { CountryEntity } from "src/app/tmc/models/CountryEntity";
 import { trigger, transition, style, animate } from "@angular/animations";
-type ITab = {
+import { DestinationAreaType } from "src/app/tmc/models/DestinationAreaType";
+import { AppHelper } from "src/app/appHelper";
+import { RefresherComponent } from "src/app/components/refresher";
+interface ITab {
   active?: boolean;
-  name: string;
+  name: "非洲" | "欧洲" | "南美洲" | "北美洲" | "大洋洲" | "亚洲";
   id: string;
-  enName: string;
-  trafficlines: TrafficlineEntity[];
-};
+  areaTypes: DestinationAreaType[];
+  enName:
+    | "Africa"
+    | "Europe"
+    | "SouthAmerica"
+    | "NorthAmerica"
+    | "Oceanica"
+    | "Asia";
+}
 @Component({
   selector: "app-select-inter-city",
   templateUrl: "./select-inter-city.page.html",
@@ -57,34 +74,29 @@ type ITab = {
 export class SelectInterCityPage implements OnInit, OnDestroy, AfterViewInit {
   private subscriptions: Subscription[] = [];
   private searchCitySubscription = Subscription.EMPTY;
-  private trafficlines: TrafficlineEntity[];
-  private pageSize = 25;
-  private isFirstInit = false;
-  searchCities: TrafficlineEntity[];
-  totalMatchCities: TrafficlineEntity[];
-  searchContinents: any[];
-  selectedCity: any;
+  private pageIndex = 0;
+  private pageSize = 20;
+  selectedCity: TrafficlineEntity;
   tabs: ITab[];
   tab: ITab;
   searchCityKeyWords = "";
-  searchCountrykeywords = "";
-  @ViewChildren("searchCountryEle") searchCountryEles: QueryList<IonSearchbar>;
+  searchResults: ISearchTextValue[];
   @ViewChildren("continentTab") continentTabs: QueryList<
     ElementRef<HTMLElement>
   >;
   @ViewChild("searchcity") searchcityEle: IonSearchbar;
   @ViewChild(IonContent) content: IonContent;
-  @ViewChild(IonInfiniteScroll) scroller: IonInfiniteScroll;
+  @ViewChild(IonInfiniteScroll, { static: true }) scroller: IonInfiniteScroll;
+  @ViewChild(RefresherComponent, { static: true })
+  refresher: RefresherComponent;
   @ViewChild(BackButtonComponent) backBtn: BackButtonComponent;
-  isLoadingContinents = false;
+  isLoading = false;
   constructor(
-    private internationalHotelService: InternationalHotelService,
-    private plt: Platform,
-    private navCtrl: NavController
+    private hotelService: InternationalHotelService,
+    private plt: Platform
   ) {}
 
   ngOnDestroy() {
-    this.isFirstInit = true;
     this.subscriptions.forEach(sub => sub.unsubscribe());
   }
   onActiveTab(tab: ITab) {
@@ -101,16 +113,15 @@ export class SelectInterCityPage implements OnInit, OnDestroy, AfterViewInit {
       );
       this.scrollTabToCenter(el && el.nativeElement);
     }
-    this.searchCities = [];
-    this.onGoToTop();
-    this.loadMoreData();
+    this.goToTop();
+    this.onSearch();
   }
   onCitySelected(city: any) {
     console.log("选择的城市", city);
     this.selectedCity = city;
     if (this.selectedCity) {
-      this.internationalHotelService.setSearchConditionSource({
-        ...this.internationalHotelService.getSearchCondition(),
+      this.hotelService.setSearchConditionSource({
+        ...this.hotelService.getSearchCondition(),
         destinationCity: this.selectedCity
       });
     }
@@ -118,73 +129,51 @@ export class SelectInterCityPage implements OnInit, OnDestroy, AfterViewInit {
       this.back();
     }, 200);
   }
-  private enableScroller(enabled = true) {
-    if (this.scroller) {
-      this.scroller.disabled = enabled;
-    }
-  }
   back() {
     this.backBtn.backToPrePage();
   }
-
-  private filterCities(cities: TrafficlineEntity[], name: string = "") {
-    cities = cities || [];
-    const keys = [
-      "Name",
-      "CityName",
-      "EnglishName",
-      "CityEnglishName",
-      // "PinYin",
-      "Nickname"
-    ];
-    name = (name || "").trim().toLowerCase();
-    let tempCities = cities;
-    if (name) {
-      tempCities = [];
-      tempCities = cities.filter(c => {
-        return keys.some(key => {
-          const value: string = (c[key] || "").trim().toLowerCase();
-          const ok = value && (value == name || value.includes(name));
-          return ok;
-        });
-      });
-    }
-    // console.log(name, tempCities);
-    this.totalMatchCities = tempCities;
-    const result = tempCities.slice(
-      this.searchCities.length,
-      this.searchCities.length + this.pageSize
-    );
-    return result;
+  private onSearch() {
+    this.pageIndex = 0;
+    this.searchResults = [];
+    this.loadMoreData();
   }
   async loadMoreData() {
-    let arr: any[];
-    const tab = this.tabs && this.tabs.find(it => it.active);
-    if (tab) {
-      arr = this.filterCities(tab.trafficlines, this.searchCityKeyWords);
-    } else {
-      arr = this.filterCities(this.trafficlines, this.searchCityKeyWords);
-    }
-    if (this.scroller) {
-      this.scroller.complete();
-    }
-    if (arr && arr.length) {
-      this.searchCities = this.searchCities.concat(arr);
-    }
-    this.enableScroller(arr.length > this.pageSize);
+    this.isLoading = this.pageIndex <= 0;
+    this.searchCitySubscription = this.hotelService
+      .searchHotelCity({
+        name: this.searchCityKeyWords,
+        areaTypes: this.tab ? this.tab.areaTypes || [] : [],
+        pageIndex: this.pageIndex,
+        pageSize: this.pageSize
+      })
+      .pipe(
+        finalize(() => {
+          if (this.pageIndex <= 1) {
+            this.refresher.complete();
+          }
+          this.isLoading = false;
+        })
+      )
+      .subscribe(res => {
+        const searchResults = (res && res.Data) || [];
+        this.scroller.disabled = searchResults.length < this.pageSize;
+        if (searchResults.length) {
+          this.pageIndex++;
+        }
+        this.scroller.complete();
+        this.searchResults = this.searchResults.concat(searchResults);
+      });
   }
   ngOnInit() {
-    this.searchCities = [];
-    setTimeout(
-      async () => {
-        await this.getTrafficlines();
-        this.isFirstInit = true;
-        this.loadMoreData();
-      },
-      !this.isFirstInit ? 500 : 0
-    );
+    this.initTabs();
+    if (this.hotelService.getSearchCondition().destinationCity) {
+      this.searchCityKeyWords = this.hotelService.getSearchCondition().destinationCity.Name;
+    }
+    this.pageIndex = 0;
+    this.searchResults = [];
+    this.loadMoreData();
   }
-  onGoToTop() {
+  private goToTop() {
     if (this.content) {
       requestAnimationFrame(async () => {
         const ele = await this.content.getScrollElement();
@@ -204,73 +193,68 @@ export class SelectInterCityPage implements OnInit, OnDestroy, AfterViewInit {
       }
     }
   }
+  doRefresh() {
+    this.searchResults = [];
+    this.pageIndex = 0;
+    this.searchCityKeyWords = "";
+    this.loadMoreData();
+  }
   onSearchCities() {
-    if (this.tabs) {
-      this.tabs = this.tabs.map(it => {
-        it.active = false;
-        return it;
-      });
-    }
     this.searchCitySubscription.unsubscribe();
     this.searchCitySubscription = of(this.searchCityKeyWords)
       .pipe(distinctUntilChanged(), debounceTime(300))
       .subscribe(_ => {
         console.log("searchcity", _);
-        this.searchCities = [];
         this.loadMoreData();
-        this.onGoToTop();
+        this.goToTop();
       });
   }
-  ngAfterViewInit() {
-    const sub = this.searchCountryEles.changes.subscribe(_ => {
-      if (this.searchCountryEles.last) {
-        setTimeout(() => {
-          this.searchCountryEles.last.setFocus();
-        }, 0);
-      }
-    });
-    this.subscriptions.push(sub);
-    if (this.searchcityEle) {
-      const sub1 = this.searchcityEle.ionFocus.subscribe(_ => {
-        if (!this.trafficlines || !this.trafficlines.length) {
-          setTimeout(
-            () => {
-              this.getTrafficlines();
-            },
-            !this.isFirstInit ? 500 : 0
-          );
-        }
-      });
-      this.subscriptions.push(sub1);
-    }
-  }
+  ngAfterViewInit() {}
 
   private initTabs() {
     this.tabs = [];
-    if (this.trafficlines) {
-      this.trafficlines.forEach(t => {
-        const desttype = t.DestinationAreaType;
-        const desttypename = t.DestinationAreaTypeName;
-        let tab = this.tabs.find(it => it.name == desttypename);
-        if (tab) {
-          tab.trafficlines.push(t);
-        } else {
-          tab = {} as any;
-          tab.id = t.DestinationAreaType;
-          tab.enName = t.DestinationAreaType;
-          tab.name = t.DestinationAreaTypeName;
-          tab.trafficlines = [t];
-          this.tabs.push(tab);
-        }
-      });
-    }
-  }
-  private async getTrafficlines() {
-    this.isLoadingContinents = true;
-    this.trafficlines = await this.internationalHotelService
-      .getTrafficlinesAsync()
-      .catch(_ => []);
-    this.isLoadingContinents = false;
-    this.initTabs();
+    this.tabs.push({
+      name: "亚洲",
+      areaTypes: [
+        DestinationAreaType.EastAsia,
+        DestinationAreaType.MiddleAsia,
+        DestinationAreaType.SoutheastAsia,
+        DestinationAreaType.WestAsia,
+        DestinationAreaType.HongKongMacaoTaiwan
+      ],
+      enName: "Asia",
+      id: AppHelper.uuid()
+    });
+    this.tabs.push({
+      name: "南美洲",
+      areaTypes: [DestinationAreaType.SouthAmerica],
+      enName: "SouthAmerica",
+      id: AppHelper.uuid()
+    });
+    this.tabs.push({
+      name: "北美洲",
+      areaTypes: [DestinationAreaType.NorthAmerica],
+      enName: "NorthAmerica",
+      id: AppHelper.uuid()
+    });
+    this.tabs.push({
+      name: "非洲",
+      areaTypes: [DestinationAreaType.Africa],
+      enName: "Africa",
+      id: AppHelper.uuid()
+    });
+    this.tabs.push({
+      name: "欧洲",
+      areaTypes: [DestinationAreaType.Europe],
+      enName: "Europe",
+      id: AppHelper.uuid()
+    });
+
+    this.tabs.push({
+      name: "大洋洲",
+      areaTypes: [DestinationAreaType.Oceanica],
+      enName: "Oceanica",
+      id: AppHelper.uuid()
+    });
   }
 }
