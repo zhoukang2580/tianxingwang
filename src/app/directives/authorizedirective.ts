a18855efab8308b030baf54f0968f1dd925f5ce8
huangmanbiao <570607278@qq.com>
2020-03-17 20:39:43 +0800

    authority

--- src/app/directives/authorizedirective.ts
+++ src/app/directives/authorizedirective.ts
@@ -6,7 +6,9 @@ import {
   TemplateRef,
   OnChanges,
   SimpleChanges,
-  ElementRef
+  ElementRef,
+  AfterContentChecked,
+  AfterViewChecked
 } from "@angular/core";
 
 @Directive({
@@ -14,12 +16,27 @@ import {
 })
 export class AuthorizeDirective implements OnChanges {
   @Input() appAuthority: string;
+  private lastTime = 0;
+  private initialDisplayStyle;
   constructor(
     private authorizeService: AuthorizeService,
-    private viewContainerRef: ViewContainerRef,
-    private tempRef: TemplateRef<any>,
+    // private viewContainerRef: ViewContainerRef,
+    // private tempRef: TemplateRef<any>,
     private el: ElementRef<HTMLElement>
-  ) {}
+  ) {
+    this.initialDisplayStyle = this.el.nativeElement.style.display;
+  }
+  // ngAfterViewChecked() {
+  //   const now = Date.now();
+  //   if (now - this.lastTime < 200) {
+  //     return;
+  //   }
+  //   console.log("ngAfterViewChecked appAuthority", this.appAuthority);
+  //   this.lastTime = now;
+  //   if (this.appAuthority) {
+  //     this.changeView();
+  //   }
+  // }
   ngOnChanges(changes: SimpleChanges) {
     if (changes.appAuthority && changes.appAuthority.currentValue) {
       this.changeView();
@@ -27,24 +44,29 @@ export class AuthorizeDirective implements OnChanges {
   }
   private async changeView() {
     try {
-      const authorizes = await this.authorizeService.getAuthorizes();
-      if (authorizes && authorizes.length) {
-        const isShow = !!authorizes.find(it => it == this.appAuthority);
-        if (isShow) {
-          this.showHide(false);
-        } else {
-          this.el.nativeElement.style.display = "none";
-          this.showHide(false);
-        }
+      const authorizes = await this.authorizeService
+        .getAuthorizes()
+        .catch(() => null);
+      if (authorizes) {
+        const isShow = !!authorizes[this.appAuthority];
+        this.showHide(isShow);
       } else {
         this.showHide(false);
       }
-    } catch (e) {}
+    } catch (e) {
+      console.error("authorizes ", e);
+    }
   }
   private showHide(isShow = false) {
+    // console.log(this.appAuthority, isShow);
     if (isShow) {
       // this.viewContainerRef.createEmbeddedView(this.tempRef);
-      this.el.nativeElement.style.display = "initial";
+      // console.log(
+      //   "initialDisplayStyle",
+      //   this.initialDisplayStyle,
+      //   this.el.nativeElement.style.display
+      // );
+      this.el.nativeElement.style.display = this.initialDisplayStyle || "";
     } else {
       // this.viewContainerRef.clear();
       this.el.nativeElement.style.display = "none";
