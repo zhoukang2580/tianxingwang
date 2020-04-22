b0625197975f4d2e13b60d20ceb6302687c3c1e3
东美在线 <dongmeizaixian@dongmeizaixiandeMacBook-Pro.local>
2020-04-22 17:35:23 +0800

    hotel

--- src/app/hotel-international/components/over-hotel/over-hotel.component.ts
+++ src/app/hotel-international/components/over-hotel/over-hotel.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TmcDataEntity } from 'src/app/tmc/models/TmcDataEntity';
+import { Router, ActivatedRoute } from '@angular/router';
+import { TmcService } from 'src/app/tmc/tmc.service';
 
 @Component({
   selector: 'app-over-hotel',
@@ -6,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./over-hotel.component.scss'],
 })
 export class OverHotelComponent implements OnInit {
+  tmc: TmcDataEntity;
+  constructor(private router: Router, private tmcService: TmcService, private route: ActivatedRoute) { }
 
-  constructor() { }
-
-  ngOnInit() {}
+  ngOnInit() {
+    this.route.queryParamMap.subscribe(async _ => {
+      this.tmc = await this.tmcService.getTmcData();
+    })
+  }
 
 }
