53f5edf789dd21b61220766dc0f5bb557ebe8a79
huangmanbiao <570607278@qq.com>
2020-04-22 17:46:27 +0800

    违规原因

--- src/app/tmc/components/book-illegal-reason-comp/book-illegal-reason-comp.component.ts
+++ src/app/tmc/components/book-illegal-reason-comp/book-illegal-reason-comp.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 import { IllegalReasonEntity } from "../../tmc.service";
+import { PopoverController } from "@ionic/angular";
+import { SelectComponent } from "src/app/components/select/select.component";
 
 @Component({
   selector: "app-book-illegal-reason-comp",
   templateUrl: "./book-illegal-reason-comp.component.html",
-  styleUrls: ["./book-illegal-reason-comp.component.scss"]
+  styleUrls: ["./book-illegal-reason-comp.component.scss"],
 })
 export class BookIllegalReasonCompComponent implements OnInit {
   @Input() disabled: boolean;
@@ -14,14 +16,34 @@ export class BookIllegalReasonCompComponent implements OnInit {
   isOtherIllegalReason: boolean;
   otherIllegalReason: string;
   illegalReason: string;
-  constructor() {
+  constructor(private popoverCtrl: PopoverController) {
     this.ionchange = new EventEmitter();
+  }
+  async onOpenSelect() {
+    const p = await this.popoverCtrl.create({
+      component: SelectComponent,
+      cssClass: "vw-70",
+      componentProps: {
+        label: "超标原因",
+        data: (this.illegalReasons || []).map((it) => {
+          return {
+            label: it.Name,
+            value: it.Name,
+          };
+        }),
+      },
+    });
+    p.present();
+    const data = await p.onDidDismiss();
+    if (data && data.data) {
+      this.illegalReason = data.data;
+    }
   }
   onValueChange() {
     this.ionchange.emit({
       isOtherIllegalReason: this.isOtherIllegalReason,
       otherIllegalReason: this.otherIllegalReason,
-      illegalReason: this.illegalReason
+      illegalReason: this.illegalReason,
     });
   }
   ngOnInit() {}
