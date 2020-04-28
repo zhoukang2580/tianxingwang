6cb4ed5d7227f1889547210ba62ae15220b7d809
huangmanbiao <570607278@qq.com>
2020-04-28 18:10:38 +0800

    我的差旅审批模块

---
+++ src/app/travel-application/routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Routes } from "@angular/router";
+
+export const routes: Routes = [];
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+})
+export class RoutingModule {}
