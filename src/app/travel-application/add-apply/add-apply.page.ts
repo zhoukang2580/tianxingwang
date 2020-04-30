f2d4050b525033f63dcc06e81a17d87d84af385a
huangmanbiao <570607278@qq.com>
2020-04-30 15:02:40 +0800

    修改

--- src/app/travel-application/add-apply/add-apply.page.ts
+++ src/app/travel-application/add-apply/add-apply.page.ts
@@ -1,21 +1,26 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import { OrganizationComponent } from 'src/app/tmc/components/organization/organization.component';
-import { OrganizationEntity, CostCenterEntity } from 'src/app/hr/staff.service';
-import { Subscription } from 'rxjs';
-import { TravelFormEntity } from 'src/app/tmc/tmc.service';
-import { SearchModel, TravelService, TravelFormTripEntity } from '../travel.service';
-import { SelectCostcenter } from '../components/select-costcenter/select-costcenter';
-import { AppHelper } from 'src/app/appHelper';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { ModalController } from "@ionic/angular";
+import { OrganizationComponent } from "src/app/tmc/components/organization/organization.component";
+import { OrganizationEntity, CostCenterEntity } from "src/app/hr/staff.service";
+import { Subscription } from "rxjs";
+import { TravelFormEntity } from "src/app/tmc/tmc.service";
+import {
+  SearchModel,
+  TravelService,
+  TravelFormTripEntity,
+} from "../travel.service";
+import { SelectCostcenter } from "../components/select-costcenter/select-costcenter";
+import { AppHelper } from "src/app/appHelper";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-add-apply',
-  templateUrl: './add-apply.page.html',
-  styleUrls: ['./add-apply.page.scss'],
+  selector: "app-add-apply",
+  templateUrl: "./add-apply.page.html",
+  styleUrls: ["./add-apply.page.scss"],
 })
 export class AddApplyPage implements OnInit, OnDestroy {
   organization: string;
+  organizationId: string;
   costCenterName: string;
   costCenterCode: string;
   organizationCode: string;
@@ -23,12 +28,15 @@ export class AddApplyPage implements OnInit, OnDestroy {
   private subscription = Subscription.EMPTY;
   items: TravelFormEntity[];
   searchModel: SearchModel;
-  constructor(private modalCtrl: ModalController,
+  constructor(
+    private modalCtrl: ModalController,
     private route: ActivatedRoute,
-    private service: TravelService, private router: Router, ) { }
+    private service: TravelService,
+    private router: Router
+  ) {}
 
   ngOnDestroy() {
-    this.subscription.unsubscribe()
+    this.subscription.unsubscribe();
   }
   ngOnInit() {
     this.searchModel = {} as any;
@@ -38,28 +46,29 @@ export class AddApplyPage implements OnInit, OnDestroy {
     this.searchModel.PageSize = 20;
     const item: TravelFormTripEntity = {} as any;
     // item.StartDate
-    this.route.queryParamMap.subscribe(q => {
+    this.route.queryParamMap.subscribe((q) => {
       if (q.get("data")) {
-        this.searchModel = JSON.parse(q.get("data"))
+        this.searchModel = JSON.parse(q.get("data"));
       }
-    })
-    this.searchModel.TravelForm.Trips.push(item)
+    });
+    this.searchModel.TravelForm.Trips.push(item);
   }
   async onSelectOrg() {
     const m = await this.modalCtrl.create({ component: OrganizationComponent });
     m.present();
     const data = await m.onDidDismiss();
-    const org = data && data.data as OrganizationEntity;
+    const org = data && (data.data as OrganizationEntity);
     if (org) {
       this.organization = org.Name;
       this.organizationCode = org.Code;
+      this.organizationId = org.Id;
     }
   }
   async onSelectCostCenter() {
     const m = await this.modalCtrl.create({ component: SelectCostcenter });
     m.present();
     const data = await m.onDidDismiss();
-    const org = data && data.data as CostCenterEntity;
+    const org = data && (data.data as CostCenterEntity);
     if (org) {
       this.costCenterName = org.Name;
       this.costCenterCode = org.Code;
@@ -69,22 +78,27 @@ export class AddApplyPage implements OnInit, OnDestroy {
     if (this.searchModel.TravelForm) {
       this.searchModel.TravelForm.Organization = {
         Code: this.organizationCode,
-        Name: this.organization
+        Name: this.organization,
+        Id: this.organizationId,
       } as OrganizationEntity;
-      this.searchModel.TravelForm.CustomerName = this.customerName
-      this.searchModel.TravelForm.CostCenterName = this.costCenterName
+      this.searchModel.OrganizationId = this.organizationId;
+      this.searchModel.TravelForm.CustomerName = this.customerName;
+      this.searchModel.TravelForm.CostCenterName = this.costCenterName;
+      this.searchModel.Trips = this.searchModel.TravelForm.Trips;
     }
-    this.service.travelSubmit(this.searchModel)
+    this.service.travelSubmit(this.searchModel);
   }
 
   onRemoveTrip(item: TravelFormTripEntity) {
     if (item && this.searchModel.TravelForm.Trips) {
-      this.searchModel.TravelForm.Trips = this.searchModel.TravelForm.Trips.filter(it => it !== item);
+      this.searchModel.TravelForm.Trips = this.searchModel.TravelForm.Trips.filter(
+        (it) => it !== item
+      );
     }
   }
   onAddTrip() {
     const item: TravelFormTripEntity = {} as any;
     // item.StartDate
-    this.searchModel.TravelForm.Trips.push(item)
+    this.searchModel.TravelForm.Trips.push(item);
   }
 }
--- src/app/travel-application/business-list/business-list.page.ts
+++ src/app/travel-application/business-list/business-list.page.ts
@@ -1,45 +1,49 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { AppHelper } from 'src/app/appHelper';
-import { Router } from '@angular/router';
-import { TravelService, SearchModel } from '../travel.service';
-import { Subscription } from 'rxjs';
-import { TravelFormEntity } from 'src/app/tmc/tmc.service';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { AppHelper } from "src/app/appHelper";
+import { Router } from "@angular/router";
+import { TravelService, SearchModel } from "../travel.service";
+import { Subscription } from "rxjs";
+import { TravelFormEntity } from "src/app/tmc/tmc.service";
 
 @Component({
-  selector: 'app-business-list',
-  templateUrl: './business-list.page.html',
-  styleUrls: ['./business-list.page.scss'],
+  selector: "app-business-list",
+  templateUrl: "./business-list.page.html",
+  styleUrls: ["./business-list.page.scss"],
 })
-export class BusinessListPage implements OnInit,OnDestroy {
-  private subscription=Subscription.EMPTY;
+export class BusinessListPage implements OnInit, OnDestroy {
+  private subscription = Subscription.EMPTY;
   items: TravelFormEntity[];
   searchModel: SearchModel;
-  constructor(private router: Router, private service: TravelService) { }
+  constructor(private router: Router, private service: TravelService) {}
 
   ngOnInit() {
-    this.searchModel={} as any;
-    this.searchModel.PageIndex=0;
-    this.searchModel.PageSize=20;
-    this.gettravel()
+    this.searchModel = {} as any;
+    this.searchModel.PageIndex = 0;
+    this.searchModel.PageSize = 20;
+    this.gettravel();
   }
-  ngOnDestroy(){
-    this.subscription.unsubscribe()
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
   goAddApply() {
     this.router.navigate([AppHelper.getRoutePath("add-apply")]);
   }
   gettravel() {
-  this.subscription=  this.service.getlist(this.searchModel).subscribe(r => {
-      this.items = r && r.Data.TravelForms|| []
-    })
+    this.subscription = this.service
+      .getlist(this.searchModel)
+      .subscribe((r) => {
+        this.items = (r && r.Data.TravelForms) || [];
+      });
   }
   doRefresh() {
-
+    this.gettravel();
   }
-  async onTravelEdit(id){
-   const m=await this.service.getTravelDetail(id)
-    this.router.navigate([AppHelper.getRoutePath("add-apply")],{queryParams:{
-      data:JSON.stringify(m)
-    }});
+  async onTravelEdit(id) {
+    const m = await this.service.getTravelDetail(id);
+    this.router.navigate([AppHelper.getRoutePath("add-apply")], {
+      queryParams: {
+        data: JSON.stringify(m),
+      },
+    });
   }
 }
