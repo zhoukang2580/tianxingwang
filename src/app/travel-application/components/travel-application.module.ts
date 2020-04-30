170f95d030d62d5d10d5364d87874409fa107fd7
huangmanbiao <570607278@qq.com>
2020-04-30 11:01:27 +0800

    报错

--- src/app/travel-application/components/travel-application.module.ts
+++ src/app/travel-application/components/travel-application.module.ts
@@ -1,18 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AddStrokeComponent } from './add-stroke/add-stroke.component';
-import { IonicModule } from '@ionic/angular';
-import { AppComponentsModule } from 'src/app/components/appcomponents.module';
-import { SelectCity } from './select-city/select-city';
-import { FormsModule } from '@angular/forms';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { AddStrokeComponent } from "./add-stroke/add-stroke.component";
+import { IonicModule } from "@ionic/angular";
+import { AppComponentsModule } from "src/app/components/appcomponents.module";
+import { SelectCity } from "./select-city/select-city";
+import { FormsModule } from "@angular/forms";
+import { SelectCostcenter } from "./select-costcenter/select-costcenter";
 
 @NgModule({
-  declarations: [AddStrokeComponent,SelectCity],
-  imports: [
-    IonicModule,CommonModule,FormsModule, AppComponentsModule
-  ],
-  exports:[AddStrokeComponent,SelectCity]
+  declarations: [AddStrokeComponent, SelectCity, SelectCostcenter],
+  imports: [IonicModule, CommonModule, FormsModule, AppComponentsModule],
+  exports: [AddStrokeComponent, SelectCity],
 })
-export class TravelApplicationComponentsModule { }
+export class TravelApplicationComponentsModule {}
