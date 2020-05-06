12d32451c5e03c7fdb276a1a90c59b18f1376704
huangmanbiao <570607278@qq.com>
2020-05-06 14:09:57 +0800

    校验规则

--- src/app/services/validator/validator.service.ts
+++ src/app/services/validator/validator.service.ts
@@ -1,77 +1,126 @@
+import { AppHelper } from "./../../appHelper";
 import { RequestEntity } from "../api/Request.entity";
 import { Injectable } from "@angular/core";
 import { ApiService } from "../api/api.service";
 import { finalize } from "rxjs/operators";
+export interface Res {
+  Message: string; // "账户编号错误"
+  Name: string; // "Account.Id"
+  Rules?: {
+    IsRange?: boolean;
+    Message?: string;
+    Options?: string;
+    Pattern?: string;
+  }[];
+}
 export interface ValidateInfo {
   name: string;
   saveType: string;
-  rule: {
-    Message: string; // "账户编号错误"
-    Name: string; // "Account.Id"
-    Rules: {
-      IsRange?: boolean;
-      Message?: string;
-      Options?: string;
-      Pattern?: string;
-    }[];
-  }[];
+  rule: Res[];
 }
 @Injectable({
-  providedIn: "root"
+  providedIn: "root",
 })
 export class ValidatorService {
   private infos: ValidateInfo[] = [];
+  private fetching: Promise<ValidateInfo>;
   constructor(private apiService: ApiService) {}
+  checkObjPropertiy(
+    obj: any,
+    name: string,
+    type: string,
+    container: HTMLElement
+  ) {
+    if (!this.infos || !this.infos.length) {
+      return "";
+    }
+    const rule = this.infos.find(
+      (it) => it.name == name && it.saveType == type
+    );
+    if (rule && rule.rule) {
+      const pro = Object.keys(obj).find((k) => !obj[k]);
+      if (pro) {
+        const oneEl = container.querySelector(
+          `[ValidateName='${pro}']`
+        ) as HTMLInputElement;
+        if (oneEl) {
+          const rect = oneEl.getBoundingClientRect();
+          if (rect) {
+            const contentEl = oneEl.closest("ion-content");
+            if (contentEl) {
+              contentEl.scrollByPoint(0, rect.bottom, 100);
+            }
+          }
+        }
+        const msg = rule.rule.find((r) => r.Name == pro);
+        if (msg && msg.Message) {
+          setTimeout(() => {
+            const el = container.querySelector(
+              `[ValidateName='${pro}']`
+            ) as HTMLInputElement;
+            if (el) {
+              el.focus();
+            }
+          }, 120);
+          return msg.Message;
+        }
+      }
+    }
+    return "";
+  }
   initialize(
     name: string,
     saveType: string,
     container: HTMLElement,
     isShowMessage: boolean = false
   ) {
-    this.get(name, saveType)
-      .then(r => {
+    return this.get(name, saveType)
+      .then((r) => {
         const validator = new window["Winner"].Validator({
           IsShowMessage: isShowMessage,
-          Style: ""
+          Style: "",
         });
         validator.Initialize();
         validator.InitializeControl(r.rule, container);
       })
-      .catch(_ => {
+      .catch((_) => {
         console.error(_);
       });
   }
-  get(name: string, saveType: string): Promise<ValidateInfo> {
-    const info = this.infos.find(s => s.name == name && s.saveType == saveType);
+  add(validateInfo: ValidateInfo) {
+    if (!validateInfo) {
+      return false;
+    }
+    const info = this.infos.find(
+      (s) => s.name == validateInfo.name && s.saveType == validateInfo.saveType
+    );
     if (info) {
+      info.rule = validateInfo.rule;
+      return true;
+    }
+    this.infos.push(validateInfo);
+    return true;
+  }
+  get(name: string, saveType: string): Promise<ValidateInfo> {
+    const info = this.infos.find(
+      (s) => s.name == name && s.saveType == saveType
+    );
+    if (info && info.rule) {
       return Promise.resolve(info);
     }
-    return new Promise<any>((resolve, reject) => {
-      const subscribtion = this.load(name, saveType)
-        .pipe(
-          finalize(() => {
-            setTimeout(() => {
-              if (subscribtion) {
-                subscribtion.unsubscribe();
-              }
-            }, 1000);
-          })
-        )
-        .subscribe(
-          r => {
-            if (r.Status && r.Data) {
-              const result = { name, saveType, rule: r.Data, validator: null };
-              this.infos.push(result);
-              resolve(result);
-            }
-            reject("");
-          },
-          error => {
-            reject(error);
-          },
-          () => {}
-        );
-    });
+    if (this.fetching) {
+      return this.fetching;
+    }
+    this.fetching = this.load(name, saveType)
+      .then((r) => {
+        const result = { name, saveType, rule: r, validator: null };
+        this.infos.push(result);
+        return result;
+      })
+      .finally(() => {
+        this.fetching = null;
+      });
+    return this.fetching;
   }
 
   private load(name: string, saveType: string) {
@@ -79,6 +128,6 @@ export class ValidatorService {
     req.Method = "ApiHomeUrl-Home-GetValidateRule";
     req.Data = JSON.stringify({ Name: name, SaveType: saveType });
     req.IsShowLoading = true;
-    return this.apiService.getResponse<any>(req);
+    return this.apiService.getPromiseData<Res[]>(req);
   }
 }
