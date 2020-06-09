5f196918d09287184206552483140c767c810ab0
东美在线 <dongmeizaixian@dongmeizaixiandeMacBook-Pro.local>
2020-06-09 13:10:12 +0800

    train

--- src/app/order/components/train-order-price-popover/train-order-price-popover.component.ts
+++ src/app/order/components/train-order-price-popover/train-order-price-popover.component.ts
@@ -12,6 +12,8 @@ import {
 import { OrderItemEntity, OrderEntity } from "../../models/OrderEntity";
 import { IonGrid, IonSlides, IonText } from "@ionic/angular";
 import { AppHelper } from "src/app/appHelper";
+import { OrderTrainTicketEntity } from '../../models/OrderTrainTicketEntity';
+import { OrderInsuranceStatusType } from "../../models/OrderInsuranceStatusType";
 
 
 @Component({
@@ -55,6 +57,46 @@ export class TrainOrderPricePopoverComponent implements OnInit {
       this.order.OrderItems.filter(
         (it) => it.Key == orderHotelKey
       ).reduce((acc, it) => (acc = AppHelper.add(acc, +it.Amount)), 0)
+    );
+  }
+  getOrderFees(t: OrderTrainTicketEntity) {
+    return (
+      this.order &&
+      this.order.VariablesJsonObj &&
+      this.order.VariablesJsonObj.orderFees[t.Id]
+    );
+  }
+  getPriceAmount(t: OrderTrainTicketEntity) {
+    this.amount = 0;
+    if (
+      this.order &&
+      this.order.VariablesJsonObj &&
+      this.order.VariablesJsonObj.orderFees[t.Id]
+    ) {
+      this.order.VariablesJsonObj.orderFees[t.Id].forEach(
+        (f) => (this.amount += f.Value)
+      );
+      if( this.getInsurances(t)){
+        this.getInsurances(t).forEach(
+          (i) => (this.amount += parseInt(i.Premium))
+        );
+      }
+    }
+    // console.log(this.amount,"this.amount");
+    return this.amount;
+  }
+  getInsurances(t: OrderTrainTicketEntity) {
+    return (
+      this.order &&
+      this.order.OrderInsurances &&
+      this.order.OrderInsurances.filter(
+        (it) => it.TravelKey == (t && t.Key)
+      ).filter(
+        (it) =>
+          it.Status != OrderInsuranceStatusType.Abolish &&
+          it.Status != OrderInsuranceStatusType.Refunded &&
+          it.Status != OrderInsuranceStatusType.PayFailure
+      )
     );
   }
 }
