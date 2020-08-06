2dadb981248be2a174e12333e3682caf1947a6a0
huangmanbiao <570607278@qq.com>
2020-08-06 12:36:57 +0800

    默认路由

--- src/app/guards/to-default-route.guard.ts
+++ src/app/guards/to-default-route.guard.ts
@@ -22,40 +22,26 @@ export class ToDefaultRouteGuard implements CanActivate {
   ): Promise<boolean> | boolean {
     const url = AppHelper.getNormalizedPath(state.url);
     console.log("to-default-route-guard url", url);
-    const tree = this.router.parseUrl(state.url);
-    const defaultUrlTree: UrlTree = {
-      root: tree.root,
-      queryParamMap: tree.queryParamMap,
-      fragment: tree.fragment,
-      queryParams: tree.queryParams,
-    };
-    defaultUrlTree.root.segments = [];
-    defaultUrlTree.root.segments.push(
-      new UrlSegment(url.substring(0, url.lastIndexOf("_")), {})
-    );
     if (this.router.config.find((it) => it.path == `${url}`)) {
       return true;
     }
-    // if (
-    //   this.router.config.find(
-    //     it => it.path == url.substring(0, url.lastIndexOf("_"))
-    //   )
-    // ) {
-    //   return false;
-    // }
-    let idx = url.lastIndexOf("_");
-    let defaultRoute = url.substring(0, idx);
-    if (this.router.config.find((it) => it.path == `${defaultRoute}`)) {
-      return true;
+    const defaultR = this.getDefaultRoute(url, state.url);
+    if (defaultR) {
+      this.router.navigate([defaultR]);
+      return false;
     }
-    idx = state.url.includes("_")
-      ? state.url.lastIndexOf("_")
-      : state.url.length;
-    defaultRoute = state.url.substring(0, idx);
-    console.log(
-      `path=${url}找不到，跳转到默认路由,defaultRoute=${defaultRoute}`
-    );
-    this.router.navigate([defaultRoute]);
-    return false;
+    return true;
+  }
+  private getDefaultRoute(styledRoute: string, toRuote: string) {
+    const idx = styledRoute.lastIndexOf("_");
+    const defaultRoute = styledRoute.substring(0, idx);
+    if (
+      defaultRoute &&
+      this.router.config.find((it) => it.path == `${defaultRoute}`)
+    ) {
+      this.router.navigate([defaultRoute]);
+      return false;
+    }
+    return defaultRoute;
   }
 }
