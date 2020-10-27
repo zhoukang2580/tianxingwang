4995c4df0a0f720d778dee5805cf72168fef5f65
huangmanbiao <570607278@qq.com>
2020-10-27 09:59:43 +0800

    css

---
+++ src/app/order/components/order-item-price-popover_en/order-item-price-popover_en.component.ts
@@ -0,0 +1,12 @@
+import {
+  Component,
+} from "@angular/core";
+import { OrderItemPricePopoverComponent } from '../order-item-price-popover/order-item-price-popover.component';
+@Component({
+  selector: "app-order-item-price-popover_en",
+  templateUrl: "./order-item-price-popover_en.component.html",
+  styleUrls: ["./order-item-price-popover_en.component.scss"],
+})
+export class OrderItemPricePopoverEnComponent extends OrderItemPricePopoverComponent {
+  
+}
