a6c3554b959476b97a4651bf36aece47fc7700f3
huangmanbiao <570607278@qq.com>
2020-12-11 17:18:24 +0800

    跳转页面

--- src/app/hotel/search-hotel-df/search-hotel-df.page.ts
+++ src/app/hotel/search-hotel-df/search-hotel-df.page.ts
@@ -403,6 +403,7 @@ export class SearchHotelDfPage
       });
       this.hotelService.getConditions();
       this.isLeavePage = true;
+      this.isOpenSelectCityPage  = true;
       this.router.navigate([AppHelper.getRoutePath("hotel-list")]);
     } else {
       this.router.navigate([
