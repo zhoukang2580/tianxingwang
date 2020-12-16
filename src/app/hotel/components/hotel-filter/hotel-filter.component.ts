caace0fe7a9939dd9ed1bff24fcc83a7e26d8d25
huangmanbiao <570607278@qq.com>
2020-12-16 09:41:46 +0800

    hotel

--- src/app/hotel/components/hotel-filter/hotel-filter.component.ts
+++ src/app/hotel/components/hotel-filter/hotel-filter.component.ts
@@ -1,5 +1,9 @@
 import { Subscription } from "rxjs";
-import { HotelQueryEntity, IFilterTabItem, IFilterTab } from "src/app/hotel/models/HotelQueryEntity";
+import {
+  HotelQueryEntity,
+  IFilterTabItem,
+  IFilterTab,
+} from "src/app/hotel/models/HotelQueryEntity";
 import { TmcService } from "src/app/tmc/tmc.service";
 import {
   Component,
@@ -9,7 +13,7 @@ import {
   Output,
   OnChanges,
   SimpleChanges,
-  OnDestroy
+  OnDestroy,
 } from "@angular/core";
 import { BrandEntity } from "src/app/hotel/models/BrandEntity";
 import { HotelConditionModel } from "src/app/hotel/models/ConditionModel";
@@ -20,14 +24,14 @@ import { HotelService } from "../../hotel.service";
 @Component({
   selector: "app-hotel-filter",
   templateUrl: "./hotel-filter.component.html",
-  styleUrls: ["./hotel-filter.component.scss"]
+  styleUrls: ["./hotel-filter.component.scss"],
 })
 export class HotelFilterComponent implements OnInit, OnDestroy {
-  @Input() langOpt ={
+  @Input() langOpt = {
     any: "不限",
     Reset: "重置",
-    determine: "确定"
-  }
+    determine: "确定",
+  };
   private subscription = Subscription.EMPTY;
   private conditionModel: HotelConditionModel;
   @Output() filter: EventEmitter<any>;
@@ -45,7 +49,7 @@ export class HotelFilterComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
   onFilter() {
-    this.modalCtrl.getTop().then(top => {
+    this.modalCtrl.getTop().then((top) => {
       if (top) {
         top.dismiss();
       }
@@ -53,11 +57,11 @@ export class HotelFilterComponent implements OnInit, OnDestroy {
     this.filter.emit();
   }
   onActive(tab: IFilterTab<any>) {
-    this.hotelQuery.filters = this.hotelQuery.filters.map(it => {
+    this.hotelQuery.filters = this.hotelQuery.filters.map((it) => {
       it.active = it.id == tab.id;
       return it;
     });
-    const active = this.hotelQuery.filters.find(it => it.active);
+    const active = this.hotelQuery.filters.find((it) => it.active);
     if (active) {
       this.items = active.items;
     }
@@ -66,11 +70,11 @@ export class HotelFilterComponent implements OnInit, OnDestroy {
     const query = this.hotelService.getHotelQueryModel();
     this.subscription = this.hotelService
       .getHotelQuerySource()
-      .subscribe(q => {
+      .subscribe((q) => {
         this.hotelQuery = q;
-        if (!this.hotelQuery || !this.hotelQuery.filters||!this.hotelQuery.filters.length) {
-          this.onReset();
-        }
+        // if (!this.hotelQuery || !this.hotelQuery.filters) {
+        //   this.onReset();
+        // }
       });
     this.conditionModel = await this.hotelService.getConditions();
     if (!query || !query.filters) {
@@ -107,19 +111,19 @@ export class HotelFilterComponent implements OnInit, OnDestroy {
     }
     const brands = this.conditionModel.Brands.slice(0, 8);
     const economy = this.conditionModel.Brands.filter(
-      it => it.Tag == "Economy"
+      (it) => it.Tag == "Economy"
     ).slice(0, 20);
 
     const comfort = this.conditionModel.Brands.filter(
-      it => it.Tag == "Comfort"
+      (it) => it.Tag == "Comfort"
     ).slice(0, 10);
 
     const high = this.conditionModel.Brands.filter(
-      it => it.Tag == "High"
+      (it) => it.Tag == "High"
     ).slice(0, 10);
 
     const luxury = this.conditionModel.Brands.filter(
-      it => it.Tag == "Luxury"
+      (it) => it.Tag == "Luxury"
     ).slice(0, 10);
 
     const tabBrand: IFilterTab<IFilterTabItem<BrandEntity>> = {
@@ -129,48 +133,48 @@ export class HotelFilterComponent implements OnInit, OnDestroy {
         {
           label: "热门品牌（可多选）",
           isMulti: true,
-          items: brands.map(it => {
+          items: brands.map((it) => {
             return { ...it } as any;
-          })
+          }),
         },
         {
           label: "经济（可多选）",
           isMulti: true,
-          items: economy.map(it => {
+          items: economy.map((it) => {
             return { ...it } as any;
-          })
+          }),
         },
         {
           label: "舒适（可多选）",
           isMulti: true,
-          items: comfort.map(it => {
+          items: comfort.map((it) => {
             return { ...it } as any;
-          })
+          }),
         },
         {
           label: "高端（可多选）",
           isMulti: true,
-          items: high.map(it => {
+          items: high.map((it) => {
             return { ...it } as any;
-          })
+          }),
         },
         {
           label: "豪华（可多选）",
           isMulti: true,
-          items: luxury.map(it => {
+          items: luxury.map((it) => {
             return { ...it } as any;
-          })
-        }
-      ]
+          }),
+        },
+      ],
     };
     this.hotelQuery.filters.push(tabBrand);
   }
   private resetTabTheme() {
-    if (!this.conditionModel.Amenities) {
+    if (!this.conditionModel || !this.conditionModel.Amenities) {
       return;
     }
     const amenities = this.conditionModel.Amenities.filter(
-      it => it.Tag == "Theme"
+      (it) => it.Tag == "Theme"
     );
     const tab: IFilterTab<IFilterTabItem<AmenityEntity>> = {
       label: "主题",
@@ -179,18 +183,18 @@ export class HotelFilterComponent implements OnInit, OnDestroy {
         {
           label: "主题类型",
           isMulti: true,
-          items: amenities
-        }
-      ]
+          items: amenities,
+        },
+      ],
     };
     this.hotelQuery.filters.push(tab);
   }
   private resetTabService() {
-    if (!this.conditionModel.Amenities) {
+    if (!this.conditionModel || !this.conditionModel.Amenities) {
       return;
     }
     const amenities = this.conditionModel.Amenities.filter(
-      it => it.Tag == "Service"
+      (it) => it.Tag == "Service"
     );
     const tab: IFilterTab<IFilterTabItem<AmenityEntity>> = {
       label: "服务",
@@ -199,18 +203,18 @@ export class HotelFilterComponent implements OnInit, OnDestroy {
         {
           label: "服务类型",
           isMulti: true,
-          items: amenities
-        }
-      ]
+          items: amenities,
+        },
+      ],
     };
     this.hotelQuery.filters.push(tab);
   }
   private resetTabFacility() {
-    if (!this.conditionModel.Amenities) {
+    if (!this.conditionModel || !this.conditionModel.Amenities) {
       return;
     }
     const amenities = this.conditionModel.Amenities.filter(
-      it => it.Tag == "Facility"
+      (it) => it.Tag == "Facility"
     );
     const tab: IFilterTab<IFilterTabItem<AmenityEntity>> = {
       label: "设施",
@@ -219,15 +223,15 @@ export class HotelFilterComponent implements OnInit, OnDestroy {
         {
           label: "设施服务",
           isMulti: true,
-          items: amenities
-        }
-      ]
+          items: amenities,
+        },
+      ],
     };
     this.hotelQuery.filters.push(tab);
   }
   resetItems(item: IFilterTabItem<BrandEntity | AmenityEntity>) {
     if (item && item.items) {
-      item.items = item.items.map(it => {
+      item.items = item.items.map((it) => {
         it.IsSelected = false;
         return it;
       });
@@ -241,22 +245,22 @@ export class HotelFilterComponent implements OnInit, OnDestroy {
     if (
       item &&
       item.items &&
-      item.items.filter(j => j.IsSelected).length >= 3
+      item.items.filter((j) => j.IsSelected).length >= 3
     ) {
       this.toastCtrl
         .create({
           message: `${item.label}不能超过3个`,
           position: "middle",
-          duration: 1000
+          duration: 1000,
         })
-        .then(t => t.present());
+        .then((t) => t.present());
       it.IsSelected = false;
       return;
     }
     if (item.isMulti) {
       it.IsSelected = !it.IsSelected;
     } else {
-      item.items.forEach(i => {
+      item.items.forEach((i) => {
         i.IsSelected = i.Id == it.Id;
       });
     }
@@ -271,10 +275,10 @@ export class HotelFilterComponent implements OnInit, OnDestroy {
   }
   private resetHasFilterItem() {
     if (this.hotelQuery && this.hotelQuery.filters) {
-      this.hotelQuery.filters = this.hotelQuery.filters.map(tab => {
+      this.hotelQuery.filters = this.hotelQuery.filters.map((tab) => {
         if (tab.items) {
           tab.hasFilterItem = tab.items.some(
-            j => j.items && j.items.some(k => k.IsSelected)
+            (j) => j.items && j.items.some((k) => k.IsSelected)
           );
         }
         return tab;
