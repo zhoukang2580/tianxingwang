589bc00c62aff191c997f89b6da4897fa2dca761
huangmanbiao <570607278@qq.com>
2021-01-22 18:22:01 +0800

    需求单

--- src/app/demand/components/demand-airport-services/demand-airport-services.component.ts
+++ src/app/demand/components/demand-airport-services/demand-airport-services.component.ts
@@ -1,29 +1,32 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { AppHelper } from 'src/app/appHelper';
-import { CalendarService } from 'src/app/tmc/calendar.service';
-import { DemandAirportServiceModel, DemandService } from '../../demand.service';
-import { DemandSearchComponent } from '../demand-search/demand-search.component';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { AppHelper } from "src/app/appHelper";
+import { CalendarService } from "src/app/tmc/calendar.service";
+import { TrafficlineEntity } from "src/app/tmc/models/TrafficlineEntity";
+import { DemandAirportServiceModel, DemandService } from "../../demand.service";
+import { DemandSearchComponent } from "../demand-search/demand-search.component";
 
 @Component({
-  selector: 'app-demand-airport-services',
-  templateUrl: './demand-airport-services.component.html',
-  styleUrls: ['./demand-airport-services.component.scss'],
+  selector: "app-demand-airport-services",
+  templateUrl: "./demand-airport-services.component.html",
+  styleUrls: ["./demand-airport-services.component.scss"],
 })
 export class DemandAirportServicesComponent implements OnInit {
-  @Input() demandAirportModel:DemandAirportServiceModel;
+  @Input() demandAirportModel: DemandAirportServiceModel;
   @Output() demandAirport: EventEmitter<any>;
+  airports: TrafficlineEntity[];
+  airport: TrafficlineEntity;
   constructor(
-    private calendarService:CalendarService,
-    private demandService:DemandService
+    private calendarService: CalendarService,
+    private demandService: DemandService
   ) {
     this.demandAirport = new EventEmitter();
-   }
+  }
 
   ngOnInit() {
     this.demandAirportModel = {} as any;
     let date = new Date();
     this.demandAirportModel.DepartureDateDay = date.toLocaleDateString();
-    this.demandAirportModel.DepartureDateHour = '12:30';
+    this.demandAirportModel.DepartureDateHour = "12:30";
   }
 
   async onSelectCitys() {
@@ -37,49 +40,66 @@ export class DemandAirportServicesComponent implements OnInit {
     if (d && d.data) {
       const c = d.data;
       this.demandAirportModel.City = `${c.Name}`;
+      this.initAirports(true, c);
     }
   }
+  private async initAirports(isFrom = true, c: TrafficlineEntity) {
+    const airports = await this.demandService.getAirports();
+    if (airports) {
+      const arr = airports.filter((it) => it.CityCode == c.Code);
+      console.log(arr, c.Code);
 
-  onSubmit(){
+      if (isFrom) {
+        this.airports = arr;
+      } else {
+        this.airports = arr;
+      }
+      this.airport = {} as any;
+    }
+  }
+  onSubmit() {
     try {
-      if(this.demandAirportModel){
-        if(this.demandAirportModel.DepartureDateDay){
-          this.demandAirportModel.DepartureDateDay = this.demandAirportModel.DepartureDateDay.replace("T", " ").substring(
-            0,
-            10
-          );
+      if (this.demandAirportModel) {
+        if (this.demandAirportModel.DepartureDateDay) {
+          this.demandAirportModel.DepartureDateDay = this.demandAirportModel.DepartureDateDay.replace(
+            "T",
+            " "
+          ).substring(0, 10);
         }
       }
-      if(this.demandAirportModel){
-        if(!this.demandAirportModel.LiaisonName){
+      if (this.airport) {
+        this.demandAirportModel.AirportName = this.airport.Name;
+      }
+      if (this.demandAirportModel) {
+        if (!this.demandAirportModel.LiaisonName) {
           AppHelper.alert("请输入联系人");
           return;
         }
-        if(!this.demandAirportModel.LiaisonPhone){
+        if (!this.demandAirportModel.LiaisonPhone) {
           AppHelper.alert("请输入联系电话");
           return;
         }
         const reg = /^1(3|4|5|6|7|8|9)\d{9}$/;
-        if (!(reg.test(this.demandAirportModel.LiaisonPhone))) {
+        if (!reg.test(this.demandAirportModel.LiaisonPhone)) {
           AppHelper.alert("电话格式不正确");
           return;
         }
-        if(!this.demandAirportModel.ServiceType ||
+        if (
+          !this.demandAirportModel.ServiceType ||
           !this.demandAirportModel.AirportName ||
           !this.demandAirportModel.City ||
           !this.demandAirportModel.NumberOfPeople ||
           !this.demandAirportModel.Terminal ||
           !this.demandAirportModel.DepartureDateDay ||
           !this.demandAirportModel.DepartureDateHour
-          ){
+        ) {
           AppHelper.alert("请完善信息");
           return;
         }
       }
-      this.demandAirport.emit({demandAirportModel:this.demandAirportModel})
+      this.demandAirport.emit({ demandAirportModel: this.demandAirportModel });
     } catch (e) {
       AppHelper.alert(e);
     }
   }
-
 }
