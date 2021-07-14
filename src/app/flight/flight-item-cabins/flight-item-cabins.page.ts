621a445a5851fd43d2f880aa482966c132520017
huangmanbiao <570607278@qq.com>
2021-07-14 15:31:02 +0800

    经济舱分类

--- src/app/flight/flight-item-cabins/flight-item-cabins.page.ts
+++ src/app/flight/flight-item-cabins/flight-item-cabins.page.ts
@@ -602,7 +602,7 @@ export class FlightItemCabinsPage implements OnInit {
     cabins.forEach((it) => {
       if (
         it.Cabin &&
-        it.Cabin.TypeName.includes("经济舱")
+        it.Cabin.Type==FlightCabinType.Y
         // &&
         // // 最低价
         // (it.Cabin.SalesPrice == `${lowestPrice}` ||
