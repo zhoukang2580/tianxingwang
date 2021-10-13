269f36de85dc2f52c5a54b0f303969c7a13973fd
huangmanbiao <570607278@qq.com>
2021-10-13 16:04:04 +0800

    同步代码

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { enableProdMode, ApplicationRef } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-
+import 'babel-polyfill'// 解决android低版本打开白屏的问题
 import { AppModule } from "./app/app.module";
 import { environment } from "./environments/environment";
 import { hmrBootstrap } from "./hmr";
@@ -22,7 +22,7 @@ import { CONFIG } from "./app/config";
 // const module = window["module"];
 try {
   AppHelper.initlizeQueryParamers();
-  processPath();
+  AppHelper.processPath();
   console.log("url,locationurl", window.location.href);
   changeAppTitle();
   initStyle();
@@ -73,93 +73,22 @@ function initStyle() {
   AppHelper.setStyle(obj.style);
   obj.language = obj.language || (obj.style == "en" ? "en" : "cn");
 }
-function processPath() {
-  const query = AppHelper.getQueryParamers();
-  const hrefPath = AppHelper.getNormalizedPath(window.location.href);
-  if (query) {
-    if (hrefPath) {
-      if (!query.path && hrefPath) {
-        query.path = hrefPath.startsWith("http") ? "" : hrefPath;
-      }
-    }
-    if (
-      query.unroutehome &&
-      (query.unroutehome as string).toLowerCase().includes("true")
-    ) {
-      if ((query.unroutehome as string).includes("#")) {
-        const [unroutehome, path] = (query.unroutehome as string).split("#");
-        query.unroutehome = unroutehome;
-        query.path = path;
-      }
-    }
-    const path2 = query.path;
-    query.path = AppHelper.getNormalizedPath(path2);
-  }
-}
-const meta = document.createElement("meta");
-const head = document.querySelector("head");
-meta.content = `upgrade-insecure-requests`;
-meta.httpEquiv = `Content-Security-Policy`;
-const bootstrap = () =>
-  platformBrowserDynamic()
-    .bootstrapModule(AppModule)
-    .then((moduleRef) => {
-      AppHelper.setHttpClient(moduleRef.injector.get(HttpClient));
-      // 为了设置模式
-      moduleRef.injector.get(ThemeService);
-      // 加载地图
-      moduleRef.injector.get(MapService);
-      AppHelper.Router = moduleRef.injector.get(Router);
-      AppHelper.ActivatedRoute = moduleRef.injector.get(ActivatedRoute);
-      AppHelper.loadingController = moduleRef.injector.get(LoadingController);
-      AppHelper.platform = moduleRef.injector.get(Platform);
-      AppHelper.setHttpClient(moduleRef.injector.get(HttpClient));
-      AppHelper.setAlertController(moduleRef.injector.get(AlertController));
-      AppHelper.setToastController(moduleRef.injector.get(ToastController));
-      AppHelper.setModalController(moduleRef.injector.get(ModalController));
-      AppHelper.setPopoverController(moduleRef.injector.get(PopoverController));
-      return moduleRef;
-    })
-    .catch((err) => {
-      console.log(err);
-      return null;
-    });
-if (environment.production) {
-  if (!environment.mockProBuild) {
-    // head.appendChild(meta);
-  }
-  enableProdMode();
-  bootstrap();
-  // if (window["VConsole"]) {
-  //   if( window['vConsole'] ){
-  //     window['vConsole'] .destroy();
-  //   }
-  //   window['vConsole'] = new window["VConsole"]();
-  // }
-} else {
-  // if (window["cordova"]||navigator.userAgent.toLowerCase().includes("iphone")) {
-  //   if (window["VConsole"]) {
-  //     if (window['vConsole']) {
-  //       window['vConsole'].destroy();
-  //     }
-  //     window['vConsole'] = new window["VConsole"]();
-  //   }
-  // }
-  // if (window["VConsole"]) {
-  //   if( window['vConsole'] ){
-  //     window['vConsole'] .destroy();
-  //   }
-  //   window['vConsole'] = new window["VConsole"]();
-  // }
-  console.log(module);
-  if (module["hot"]) {
-    hmrBootstrap(module, bootstrap);
-  } else {
-    console.log("Amm..HMR is not enabled for webpack");
-    bootstrap().then((moduleRef) => {
-      const applicationRef = moduleRef.injector.get(ApplicationRef);
-      const appComponent = applicationRef.components[0];
-      enableDebugTools(appComponent);
-    });
-  }
-}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then((moduleRef) => {
+    AppHelper.setHttpClient(moduleRef.injector.get(HttpClient));
+    // 为了设置模式
+    moduleRef.injector.get(ThemeService);
+    AppHelper.Router = moduleRef.injector.get(Router);
+    AppHelper.ActivatedRoute = moduleRef.injector.get(ActivatedRoute);
+    AppHelper.loadingController = moduleRef.injector.get(LoadingController);
+    AppHelper.platform = moduleRef.injector.get(Platform);
+    AppHelper.setHttpClient(moduleRef.injector.get(HttpClient));
+    AppHelper.setAlertController(moduleRef.injector.get(AlertController));
+    AppHelper.setToastController(moduleRef.injector.get(ToastController));
+    AppHelper.setModalController(moduleRef.injector.get(ModalController));
+    AppHelper.setPopoverController(moduleRef.injector.get(PopoverController));
+    return moduleRef;
+  })
+  .catch((err) => console.log(err));
