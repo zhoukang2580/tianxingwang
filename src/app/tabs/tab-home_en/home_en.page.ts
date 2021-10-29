d35877d1bdb00d1c3fe31d70c1ba8538c92468aa
zhoukang <zk710193@163.com>
2021-10-29 09:53:52 +0800

    home_en

--- src/app/tabs/tab-home_en/home_en.page.ts
+++
@@ -1,9 +0,0 @@
-import { TmcHomePage } from '../tab-tmc-home/tmc-home.page';
-import { Component } from '@angular/core';
-
-@Component({
-  selector: "app-home",
-  templateUrl: "home_en.page.html",
-  styleUrls: ["home_en.page.scss"]
-})
-export class HomeEnPage extends TmcHomePage {}
